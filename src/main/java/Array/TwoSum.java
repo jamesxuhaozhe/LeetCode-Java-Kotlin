9ccb75bbe3b22fea01a10fb8c82fbfb8bc395089
Xu Haozhe <xuhaozhe@ibike668.com>
2019-04-10 10:50:37 +0800

    [Format] reformat the code to make it a little bit cleaner

--- src/main/java/Array/TwoSum.java
+++ src/main/java/Array/TwoSum.java
@@ -22,7 +22,7 @@ public class TwoSum {
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement) && (map.get(complement) != i)) {
-                return new int[]{i , map.get(complement)};
+                return new int[]{i, map.get(complement)};
             }
             map.put(nums[i], i);
         }
