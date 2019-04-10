c74ac7db9ed56ba24b2fb2dee8911e55ef49398b
Xu Haozhe <xuhaozhe@ibike668.com>
2019-04-10 09:37:28 +0800

    [Array] Optimize Java solution for TwoSum using one pass way

--- src/main/java/Array/TwoSum.java
+++ src/main/java/Array/TwoSum.java
@@ -20,14 +20,11 @@ public class TwoSum {
         }
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-        }
-
-        for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement) && (map.get(complement) != i)) {
-                return new int[]{i, map.get(complement)};
+                return new int[]{i , map.get(complement)};
             }
+            map.put(nums[i], i);
         }
         throw new IllegalArgumentException("no solution ");
     }
