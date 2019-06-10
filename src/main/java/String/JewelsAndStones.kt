48d236ccb69ce1c8899d7718a9bd504385d385ef
Xu Haozhe <xuhaozhe@ibike668.com>
2019-06-10 16:40:03 +0800

    [Kotlin] Add Java and Kotlin solution for Jewels and Stones problem

---
+++ src/main/java/String/JewelsAndStones.kt
@@ -0,0 +1,21 @@
+package String
+
+class JewelsAndStones {
+
+    fun numJewelsInStones(J: String, S: String): Int {
+        val types = J.toCharArray()
+        val typeSets = HashSet<Int>()
+        for (type in types) {
+            typeSets.add(type.toInt())
+        }
+        val stones = S.toCharArray()
+        var result = 0
+        for (stone in stones) {
+            val stoneInt = stone.toInt()
+            if (typeSets.contains(stoneInt)) {
+                result++
+            }
+        }
+        return result
+    }
+}
