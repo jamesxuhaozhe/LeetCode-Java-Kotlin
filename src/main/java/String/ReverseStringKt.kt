cfc75c3727eb878306ee9808eb23c1f41b8189a0
James Xu <jamesxuhaozhe@gmail.com>
2019-06-29 13:16:35 +0800

    Add multiple solution for both java and kotlin

---
+++ src/main/java/String/ReverseStringKt.kt
@@ -0,0 +1,20 @@
+package String
+
+class ReverseStringKt {
+    fun reverseString(s: CharArray): Unit {
+        if (s.isEmpty()) {
+            return
+        }
+
+        var i = 0
+        var j = s.size - 1
+        var middle = 'a'
+        while (i < j) {
+            middle = s[j]
+            s[j] = s[i]
+            s[i] = middle
+            i++
+            j--
+        }
+    }
+}
