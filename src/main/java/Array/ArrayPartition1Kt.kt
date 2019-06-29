879d312bc7c0cd94502b4a34d28081f28b83b90d
James Xu <jamesxuhaozhe@gmail.com>
2019-06-29 21:17:12 +0800

    [Kotlin/Java] Add bunch of kt and Java solutions for array problems

---
+++ src/main/java/Array/ArrayPartition1Kt.kt
@@ -0,0 +1,14 @@
+package Array
+
+import java.util.*
+
+class ArrayPartition1Kt {
+    fun arrayPairSum(nums: IntArray): Int {
+        Arrays.sort(nums)
+        var sum = 0
+        for (index in 0 until nums.size step 2) {
+            sum += nums[index]
+        }
+        return sum
+    }
+}
---
+++ src/main/java/Array/HeightCheckerKt.kt
@@ -0,0 +1,22 @@
+package Array
+
+import java.util.*
+
+
+class HeightCheckerKt {
+
+    fun heightChecker(heights: IntArray): Int {
+        if (heights.isEmpty()) {
+            return 0
+        }
+        val helperArray = heights.copyOf(heights.size)
+        Arrays.sort(helperArray)
+        var diff = 0
+        for (index in heights.indices) {
+            if (heights[index] != helperArray[index]) {
+                diff++
+            }
+        }
+        return diff
+    }
+}
