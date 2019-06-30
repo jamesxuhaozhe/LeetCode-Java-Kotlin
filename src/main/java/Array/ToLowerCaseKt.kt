e0db586d2eedc9ef7e850797a8da568dafa4ab80
James Xu <jamesxuhaozhe@gmail.com>
2019-06-30 23:34:31 +0800

    [Kotlin] add kt solution for to lower case problem

---
+++ src/main/java/Array/ToLowerCaseKt.kt
@@ -0,0 +1,14 @@
+package Array
+
+class ToLowerCaseKt {
+    fun toLowerCase(str: String): String {
+        val offset = 'A' - 'a'
+        val chars = str.toCharArray()
+        for (index in chars.indices) {
+            if (chars[index] in 'A'..'Z') {
+                chars[index] = chars[index] - offset
+            }
+        }
+        return String(chars)
+    }
+}
