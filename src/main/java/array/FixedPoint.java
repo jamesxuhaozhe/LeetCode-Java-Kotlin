39bf67207a18ac4e5dc9ca76eb0974e0c3b3918c
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-07-10 13:37:24 +0800

    Add Java solution for Fixed point and defang ip address

---
+++ src/main/java/array/FixedPoint.java
@@ -0,0 +1,15 @@
+package array;
+
+public class FixedPoint {
+    public int fixedPoint(int[] A) {
+        if (A == null || A.length == 0) {
+            return -1;
+        }
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == i) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/string/DefangIPAddress.java
@@ -0,0 +1,24 @@
+package string;
+
+public class DefangIPAddress {
+    public String defangIPaddr(String address) {
+        if (address == null || address.isEmpty()) {
+            return null;
+        }
+
+        char[] chars = address.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char ch : chars) {
+            if (ch == '.') {
+                sb.append("[.]");
+            } else {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new DefangIPAddress().defangIPaddr("139.168.34.56"));
+    }
+}
