1a9b885edd3b52d1cdc0cf11c89ea47eceb97d49
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-07-16 21:48:00 +0800

    Add file for valid word abbr problem, not start the imple yet

---
+++ src/main/java/hashtable/ValidWordAbbr.java
@@ -0,0 +1,19 @@
+/*
+package hashtable;
+
+import ktplayground.String;
+
+public class ValidWordAbbr {
+    public ValidWordAbbr(String[] dictionary) {
+
+    }
+
+    public boolean isUnique(String word) {
+
+    }
+
+    private String genAbbrString(String str) {
+        if (str.)
+    }
+}
+*/
