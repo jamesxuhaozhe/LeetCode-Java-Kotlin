a728d49ff26edd6e5854bc7fd8780b4d2fe4515e
James Xu <jamesxuhaozhe@gmail.com>
2019-07-17 23:04:51 +0800

    Add Kotlin solution for fixed point problem

---
+++ src/main/java/array/FixedPointKt.kt
@@ -0,0 +1,15 @@
+package array
+
+class FixedPointKt {
+    fun fixedPoint(A: IntArray): Int {
+        if (A.isEmpty()) {
+            return -1
+        }
+        for (index in A.indices) {
+            if (index == A[index]) {
+                return index
+            }
+        }
+        return -1
+    }
+}
