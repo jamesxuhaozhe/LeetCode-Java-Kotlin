ab8cc659c2c487248b454929a0182b277334c031
James Xu <jamesxuhaozhe@gmail.com>
2019-07-22 22:32:48 +0800

    Add numbers of days in a month

---
+++ src/main/java/math/NumbersOfDaysInAMonth.java
@@ -0,0 +1,16 @@
+package math;
+
+public class NumbersOfDaysInAMonth {
+
+    private static final int[] LEAP_YEAR = new int[]{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    private static final int[] NON_LEAP_YEAR = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public int numberOfDays(int Y, int M) {
+        if ((Y % 100 != 0 && Y % 4 == 0) || (Y % 100 == 0 && Y % 400 == 0)) {
+            return LEAP_YEAR[M - 1];
+        } else {
+            return NON_LEAP_YEAR[M - 1];
+        }
+    }
+}
