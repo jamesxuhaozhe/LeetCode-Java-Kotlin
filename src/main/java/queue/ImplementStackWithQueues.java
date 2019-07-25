f08487abae61c96f5740ee1c6e04b60c4e9c76d6
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-07-25 22:08:27 +0800

    Add some Stack and Queue solution

---
+++ src/main/java/queue/ImplementStackWithQueues.java
@@ -0,0 +1,48 @@
+package queue;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+//TODO need to fix the logic, the below logic is not right.
+public class ImplementStackWithQueues {
+
+    private Queue<Integer> input;
+
+    private Queue<Integer> output;
+
+    /** Initialize your data structure here. */
+    public ImplementStackWithQueues() {
+        input = new PriorityQueue<>();
+        output = new PriorityQueue<>();
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+        input.offer(x);
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        transfer();
+        return output.poll();
+    }
+
+    /** Get the top element. */
+    public int top() {
+        transfer();
+        return output.poll();
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return input.isEmpty() && output.isEmpty();
+    }
+
+    private void transfer() {
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.offer(input.poll());
+            }
+        }
+    }
+}
