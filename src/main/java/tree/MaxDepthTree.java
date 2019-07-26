a6f5373cc314119dd03e97da41ed99b82949fab9
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-07-26 15:21:33 +0800

    Add max depth tree Java solution

---
+++ src/main/java/tree/MaxDepthTree.java
@@ -0,0 +1,14 @@
+package tree;
+
+import misc.datastructure.TreeNode;
+
+public class MaxDepthTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftMaxDepth = maxDepth(root.left);
+        int rightMaxDepth = maxDepth(root.right);
+        return Math.max(leftMaxDepth, rightMaxDepth) + 1;
+    }
+}
