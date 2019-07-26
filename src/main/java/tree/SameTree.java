aadd38c9d3bbf2a70105ca6392d37668a1d77846
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-07-26 14:35:32 +0800

    Add same true problem Java solution

---
+++ src/main/java/tree/SameTree.java
@@ -0,0 +1,21 @@
+package tree;
+
+import misc.datastructure.TreeNode;
+
+public class SameTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+    }
+}
