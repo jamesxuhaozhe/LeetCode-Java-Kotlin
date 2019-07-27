dfbc2662190d0accb0782cf35431b9a8ebe0798a
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-07-27 21:47:26 +0800

    Add pre order binary tree traversal using Kotlin language

---
+++ src/main/java/tree/BinaryTreePreOrderTraversalKt.kt
@@ -0,0 +1,22 @@
+package tree
+
+import misc.ktdatastructure.TreeNode
+
+class BinaryTreePreOrderTraversalKt {
+    fun preorderTraversal(root: TreeNode?): List<Int> {
+        if (root == null) {
+            return emptyList()
+        }
+        val result = arrayListOf<Int>()
+        preOrderTraversal(root, result)
+        return result
+    }
+
+    private fun preOrderTraversal(node: TreeNode?, result: MutableList<Int>) {
+        if (node != null) {
+            result.add(node.`val`)
+            preOrderTraversal(node.left, result)
+            preOrderTraversal(node.right, result)
+        }
+    }
+}
