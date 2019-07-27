c8b981465273e7c0e6267ca82ab13ac76103dcd3
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-07-27 21:52:42 +0800

    Add binary tree post order traversal using Kotlin

---
+++ src/main/java/tree/BinaryTreePostOrderTraversal.kt
@@ -0,0 +1,22 @@
+package tree
+
+import misc.ktdatastructure.TreeNode
+
+class BinaryTreePostOrderTraversal {
+    fun postorderTraversal(root: TreeNode?): List<Int> {
+        if (root == null) {
+            return emptyList()
+        }
+        val result = arrayListOf<Int>()
+        postOrderTraversal(root, result)
+        return result
+    }
+
+    private fun postOrderTraversal(node: TreeNode?, result: MutableList<Int>) {
+        if (node != null) {
+            postOrderTraversal(node.left, result)
+            postOrderTraversal(node.right, result)
+            result.add(node.`val`)
+        }
+    }
+}
