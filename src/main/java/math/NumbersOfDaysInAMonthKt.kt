de04a7ac51d477d5b0cfacb765657bd434ce73cc
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-07-29 09:12:44 +0800

    Change the Kotlin syntax a little bit

--- src/main/java/math/NumbersOfDaysInAMonthKt.kt
+++ src/main/java/math/NumbersOfDaysInAMonthKt.kt
@@ -7,10 +7,10 @@ class NumbersOfDaysInAMonthKt {
         val NON_LEAP_YEAR = intArrayOf(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
     }
     fun numberOfDays(Y: Int, M: Int): Int {
-        if ((Y % 100 != 0 && Y % 4 == 0) || (Y % 100 == 0 && Y % 400 == 0)) {
-            return LEAP_YEAR[M - 1]
+        return if ((Y % 100 != 0 && Y % 4 == 0) || (Y % 100 == 0 && Y % 400 == 0)) {
+            LEAP_YEAR[M - 1]
         } else {
-            return NON_LEAP_YEAR[M - 1]
+            NON_LEAP_YEAR[M - 1]
         }
     }
 }
