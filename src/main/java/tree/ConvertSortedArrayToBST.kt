681075abd797b5ae498fcd7e67b713e7ed4a8396
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-07-31 13:10:11 +0800

    Add Kotlin solution for convert sorted array to bst problem

---
+++ src/main/java/tree/ConvertSortedArrayToBST.kt
@@ -0,0 +1,24 @@
+package tree
+
+import misc.ktdatastructure.TreeNode
+
+class ConvertSortedArrayToBST {
+
+    fun sortedArrayToBST(nums: IntArray): TreeNode? {
+        if (nums.isEmpty()) {
+            return null
+        }
+        return createBST(nums, 0, nums.size - 1)
+    }
+
+    private fun createBST(nums: IntArray, start: Int, end: Int): TreeNode? {
+        if (end < start) {
+            return null
+        }
+        val mid = (start + end) / 2
+        val node = TreeNode(nums[mid])
+        node.left = createBST(nums, start, mid - 1)
+        node.right = createBST(nums, mid + 1, end)
+        return node
+    }
+}
