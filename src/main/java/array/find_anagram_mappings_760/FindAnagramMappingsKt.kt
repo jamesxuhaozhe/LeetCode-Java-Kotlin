e71aa4cdaeb2a5a7de07fa234959e6b8c9176e06
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-08-03 21:03:49 +0800

    Add solution for find anagram mapping problem

---
+++ src/main/java/array/find_anagram_mappings_760/FindAnagramMappingsKt.kt
@@ -0,0 +1,18 @@
+package array.find_anagram_mappings_760
+
+class FindAnagramMappingsKt {
+
+    fun anagramMappings(A: IntArray, B: IntArray): IntArray {
+        val map = HashMap<Int, Int>()
+        for (index in B.indices) {
+            map[B[index]] = index
+        }
+
+        val p = IntArray(A.size)
+        for (index in A.indices) {
+            p[index] = map[A[index]]!!
+        }
+
+        return p
+    }
+}
