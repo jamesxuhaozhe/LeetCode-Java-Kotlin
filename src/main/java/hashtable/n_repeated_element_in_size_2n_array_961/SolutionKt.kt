3410a13651ff5f1e3b3018b17517c9dcbdf20d39
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-08-05 11:56:11 +0800

    Add solution for n repeated element in size 2n array

---
+++ src/main/java/hashtable/n_repeated_element_in_size_2n_array_961/SolutionKt.kt
@@ -0,0 +1,27 @@
+package hashtable.n_repeated_element_in_size_2n_array_961
+
+/**
+ * Problem link: https://leetcode-cn.com/problems/n-repeated-element-in-size-2n-array/
+ *
+ * Time complexity: O(n)
+ *
+ * Space complexity: O(n)
+ */
+class SolutionKt {
+
+    fun repeatedNTimes(A: IntArray): Int {
+        if (A.isEmpty()) {
+            return -1
+        }
+
+        val set = HashSet<Int>()
+        for (num in A) {
+            if (set.contains(num)) {
+                return num
+            }
+            set.add(num)
+        }
+
+        return -1
+    }
+}
