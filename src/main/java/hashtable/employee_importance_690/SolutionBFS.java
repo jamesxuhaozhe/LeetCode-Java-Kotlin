211df8728cb4f277a1b1fd1b41b04bbae5e1831f
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-08-07 16:38:02 +0800

    Add Java solution for employee importance

--- src/main/java/hashtable/employee_importance_690/SolutionBFS.java
+++ src/main/java/hashtable/employee_importance_690/SolutionBFS.java
@@ -5,23 +5,29 @@ import java.util.*;
 public class SolutionBFS {
 
     public int getImportance(List<Employee> employees, int id) {
+        //dump all the employee info into a map which is a helpful look up data structure
         Map<Integer, Employee> map = new HashMap<>();
         for (Employee employee : employees) {
             map.put(employee.id, employee);
         }
 
+        // building a query queue
         Queue<Integer> employeeIdQueue = new PriorityQueue<>();
         employeeIdQueue.addAll(map.get(id).subordinates);
+
+        int currentIdEmployeeImportance = map.get(id).importance;
         if (employeeIdQueue.isEmpty()) {
-            return map.get(id).importance;
+            return currentIdEmployeeImportance;
         }
 
-        int importanceSum = map.get(id).importance;
+        int importanceSum = currentIdEmployeeImportance;
         while (!employeeIdQueue.isEmpty()) {
             Employee employee = map.get(employeeIdQueue.poll());
             importanceSum += employee.importance;
             for (Integer employeeId : employee.subordinates) {
-                employeeIdQueue.addAll(map.get(employeeId).subordinates);
+                Employee curEmployee = map.get(employeeId);
+                importanceSum += curEmployee.importance;
+                employeeIdQueue.addAll(curEmployee.subordinates);
             }
         }
 
@@ -36,10 +42,22 @@ public class SolutionBFS {
         public int importance;
         // the id of direct subordinates
         public List<Integer> subordinates;
+
+        Employee(int id, int importance, List<Integer> subordinates) {
+            this.id = id;
+            this.importance = importance;
+            this.subordinates = subordinates;
+        }
     }
 
     public static void main(String[] args) {
+        Employee employee1 = new Employee(1, 5, Arrays.asList(2, 3));
+        Employee employee2 = new Employee(2, 3, Arrays.asList(4));
+        Employee employee3 = new Employee(3, 4, Collections.emptyList());
+        Employee employee4 = new Employee(4, 1, Collections.emptyList());
 
+        List<Employee> list = Arrays.asList(employee1, employee2, employee3, employee4);
+        System.out.println(new SolutionBFS().getImportance(list, 1));
     }
 
 }
