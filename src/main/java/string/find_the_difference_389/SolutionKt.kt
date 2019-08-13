8d82952200e4a38f634a1d2741932a090c7199e0
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-08-13 23:31:35 +0800

    Add Java and Kotlin solution for find the difference problem

---
+++ src/main/java/string/find_the_difference_389/SolutionKt.kt
@@ -0,0 +1,25 @@
+package string.find_the_difference_389
+
+/**
+ * Problem link: https://leetcode-cn.com/problems/find-the-difference/
+ *
+ * Time complexity: O(n)
+ *
+ * Space complexity: O(1)
+ */
+class SolutionKt {
+
+    fun findTheDifference(s: String, t: String): Char {
+        val alpha = IntArray(26)
+
+        s.forEach { alpha[it - 'a']++ }
+        t.forEach {
+            alpha[it - 'a']--
+            if (alpha[it - 'a'] < 0) {
+                return it
+            }
+        }
+
+        throw IllegalArgumentException()
+    }
+}
