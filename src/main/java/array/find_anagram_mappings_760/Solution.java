941f5a28f04ae021058a7d09bd6bfd29f0ca7cc0
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-08-17 21:34:08 +0800

    Add some change

--- src/main/java/array/find_anagram_mappings_760/Solution.java
+++ src/main/java/array/find_anagram_mappings_760/Solution.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class Solution {
 
     public int[] anagramMappings(int[] A, int[] B) {
+
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < B.length; i++) {
             map.put(B[i], i);
