d6d6a9ef22f9c10f31ef1671f3cea483eae7cccc
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-08-21 23:21:20 +0800

    Add roman to integer problem

---
+++ src/main/java/string/roman_to_integer_3/Solution.java
@@ -0,0 +1,51 @@
+package string.roman_to_integer_3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Problem link: https://leetcode-cn.com/problems/roman-to-integer/
+ * <p>
+ * Time complexity: O(n)
+ * <p>
+ * Space complexity: O(1)
+ */
+public class Solution {
+
+    private static final Map<String, Integer> map = new HashMap<>();
+
+    static {
+        map.put("I", 1);
+        map.put("V", 5);
+        map.put("X", 10);
+        map.put("L", 50);
+        map.put("C", 100);
+        map.put("D", 500);
+        map.put("M", 1000);
+        map.put("IV", 4);
+        map.put("IX", 9);
+        map.put("XL", 40);
+        map.put("XC", 90);
+        map.put("CD", 400);
+        map.put("CM", 900);
+    }
+
+    public int romanToInt(String s) {
+        if (s == null || s.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        int result = 0;
+        for (int i = 0; i < s.length(); ) {
+            if (i + 1 < s.length() && map.containsKey(s.substring(i, i + 2))) {
+                result += map.get(s.substring(i, i + 2));
+                i += 2;
+            } else {
+                result += map.get(s.substring(i, i + 1));
+                i++;
+            }
+        }
+
+        return result;
+    }
+}
