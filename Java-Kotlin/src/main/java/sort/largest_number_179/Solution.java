61e0ab841fb0a33e3da0fb96f70dee0db83b1c8b
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-08-26 23:57:08 +0800

    largest number

---
+++ Java-Kotlin/src/main/java/sort/largest_number_179/Solution.java
@@ -0,0 +1,34 @@
+package sort.largest_number_179;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Solution {
+
+    public String largestNumber(int[] nums) {
+        String[] numStr = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            numStr[i] = String.valueOf(nums[i]);
+        }
+
+        Arrays.sort(numStr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String order1 = o1 + o2;
+                String order2 = o2 + o1;
+                return order2.compareTo(order1);
+            }
+        });
+
+        if (numStr.length != 0 && numStr[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String str : numStr) {
+            sb.append(str);
+        }
+
+        return sb.toString();
+    }
+}
---
+++ Java-Kotlin/src/main/java/sort/largest_number_179/SolutionKt.kt
@@ -0,0 +1,7 @@
+package sort.largest_number_179
+
+//TODO finish this
+class SolutionKt {
+
+
+}
