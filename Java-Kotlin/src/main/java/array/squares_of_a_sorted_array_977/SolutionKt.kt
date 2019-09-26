ac0e23bf19b7dc77cd2704ba589de5f0ea67cc7e
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-09-26 09:04:04 +0800

    Add Javadoc for kt solution problem 977

--- Java-Kotlin/src/main/java/array/squares_of_a_sorted_array_977/SolutionKt.kt
+++ Java-Kotlin/src/main/java/array/squares_of_a_sorted_array_977/SolutionKt.kt
@@ -2,6 +2,15 @@ package array.squares_of_a_sorted_array_977
 
 import java.util.*
 
+/**
+ * Problem link: https://leetcode.com/problems/squares-of-a-sorted-array/
+ * <p>
+ * SolutionDFS idea: naive idea
+ * <p>
+ * Time complexity: O(nlog(n))
+ * <p>
+ * Space complexity: O(n)
+ */
 class SolutionKt {
 
     fun sortedSquares(A: IntArray): IntArray {
