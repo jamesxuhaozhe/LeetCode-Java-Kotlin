8270af700252827ecbcda1dc678ef1d6e3bfc637
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-09-27 23:19:04 +0800

    Add some Java doc for problem 169

--- Java-Kotlin/src/main/java/array/majority_element_169/SolutionKt.kt
+++ Java-Kotlin/src/main/java/array/majority_element_169/SolutionKt.kt
@@ -2,6 +2,13 @@ package array.majority_element_169
 
 import java.util.*
 
+/**
+ * Problem link: https://leetcode-cn.com/problems/majority-element/
+ *
+ * Time complexity: O(nlogn)
+ *
+ * Space complexity:
+ */
 class SolutionKt {
 
     fun majorityElement(nums: IntArray): Int {
