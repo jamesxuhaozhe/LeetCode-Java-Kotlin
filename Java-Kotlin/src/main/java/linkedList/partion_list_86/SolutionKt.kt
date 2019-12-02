72ce9d733616618666179134faa2d6a076e6b04c
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-12-02 10:37:33 +0800

    Add problem 86

---
+++ Java-Kotlin/src/main/java/linkedList/partion_list_86/SolutionKt.kt
@@ -0,0 +1,33 @@
+package linkedList.partion_list_86
+
+import datastructure.ListNodeKt
+
+class SolutionKt {
+
+    fun partition(head: ListNodeKt?, x: Int): ListNodeKt? {
+
+        if (head == null || head.next == null) {
+            return head
+        }
+        var head = head
+        var less = ListNodeKt(-1)
+        var greater = ListNodeKt(-1)
+
+        var left = less
+        var right = greater
+        less.next = head
+        while (head != null) {
+            if (head.`val` < x) {
+                less.next = head
+                less = less.next!!
+            } else {
+                greater.next = head
+                greater = greater.next!!
+            }
+            head = head.next
+        }
+        greater.next = null
+        less.next = right.next
+        return left.next
+    }
+}
