c1cd6859036a8ad96fd148fd002525ec9f8f02d2
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-12-03 12:49:32 +0800

    Add problem 82

---
+++ Java-Kotlin/src/main/java/linkedList/delete_dup_nodes_82/SolutionKt.kt
@@ -0,0 +1,31 @@
+package linkedList.delete_dup_nodes_82
+
+import datastructure.ListNodeKt
+
+class SolutionKt {
+
+    fun deleteDuplicates(head: ListNodeKt?): ListNodeKt? {
+
+        if (head == null) {
+            return head
+        }
+
+        var fakeHead = ListNodeKt(-1)
+        fakeHead.next = head
+        var pre = fakeHead
+        var curr = head
+        while (curr != null) {
+            while (curr!!.next != null && curr.`val` == curr.next!!.`val`) {
+                curr = curr.next
+            }
+            if (pre.next == curr) {
+                pre = pre.next!!
+            } else {
+                pre.next = curr.next
+            }
+            curr = curr.next
+        }
+
+        return fakeHead.next
+    }
+}
