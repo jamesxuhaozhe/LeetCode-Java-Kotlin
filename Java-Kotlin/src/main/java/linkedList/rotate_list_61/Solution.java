ae28a3c56f166940977e7ee4b8b82e6c6be2f9c8
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-12-04 11:15:43 +0800

    Add pointer to solution 61

--- Java-Kotlin/src/main/java/linkedList/rotate_list_61/Solution.java
+++ Java-Kotlin/src/main/java/linkedList/rotate_list_61/Solution.java
@@ -5,6 +5,7 @@ import datastructure.ListNode;
 public class Solution {
 
     public ListNode rotateRight(ListNode head, int k) {
+        // just do nothing
         if (k == 0) {
             return head;
         }
@@ -16,31 +17,39 @@ public class Solution {
         ListNode curr = head;
         ListNode lastNode = null;
 
+        // find the length of the list
         int len = 0;
         while (curr != null) {
             if (curr.next == null) {
+                // grab the pointer to the last node
                 lastNode = curr;
             }
             curr = curr.next;
             len++;
         }
 
+        // just do nothing
         if (k >= len && len % k == 0) {
             return head;
         }
 
+        // find the real k we need to rotate
         if (k > len) {
             k = k % len;
         }
 
         int moves = len - k;
+        // move the pointer to just one node before the node in question
         curr = head;
         while (moves-- > 1) {
             curr = curr.next;
         }
 
+        // have the last node point to the head
         lastNode.next = head;
         ListNode newHead = curr.next;
+
+        // have the cut node point to null
         curr.next = null;
         return newHead;
     }
