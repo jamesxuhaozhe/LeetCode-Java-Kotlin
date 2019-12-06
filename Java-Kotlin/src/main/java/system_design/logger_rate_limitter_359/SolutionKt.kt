cfbafa096d4f761bfcf669574263b8cf36c36b2f
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-12-06 10:08:04 +0800

    finish 369 kt solution

--- Java-Kotlin/src/main/java/system_design/logger_rate_limitter_359/SolutionKt.kt
+++ Java-Kotlin/src/main/java/system_design/logger_rate_limitter_359/SolutionKt.kt
@@ -2,7 +2,22 @@ package system_design.logger_rate_limitter_359
 
 class SolutionKt {
 
+    private val map = HashMap<String, Int>()
 
+    fun shouldPrintMessage(timestamp: Int, message: String): Boolean {
 
+        if (message in map) {
+            val lastPrintTimestamp = map[message]!!
+            if (timestamp - lastPrintTimestamp < 10) {
+                return false
+            } else {
+                map[message] = timestamp
+                return true
+            }
+        }
+
+        map[message] = timestamp
+        return true
+    }
 
 }
