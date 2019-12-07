463ccab4743b83e621a1f8bdfa20a96ef9c1963f
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-12-07 18:49:19 +0800

    Add java solution for problem 203

---
+++ Java-Kotlin/src/main/java/linkedList/remove_linkedlist_element_203/Solution.java
@@ -0,0 +1,22 @@
+package linkedList.remove_linkedlist_element_203;
+
+import datastructure.ListNode;
+
+public class Solution {
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode curr = head;
+        ListNode prev = dummy;
+        while(curr != null) {
+            if (curr.val == val) {
+                prev.next = curr.next;
+            } else {
+                prev = prev.next;
+            }
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
