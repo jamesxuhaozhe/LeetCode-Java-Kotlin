818aff2abd79c74e8593989fd4066b19a4a127a0
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-12-08 22:36:35 +0800

    java solution 705

---
+++ Java-Kotlin/src/main/java/system_design/design_hashset_705/Solution.java
@@ -0,0 +1,29 @@
+package system_design.design_hashset_705;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    private final Map<Integer, Integer> map;
+
+    /** Initialize your data structure here. */
+    public Solution() {
+        this.map = new HashMap<>();
+    }
+
+    public void add(int key) {
+        map.put(key, 0);
+    }
+
+    public void remove(int key) {
+        if (map.containsKey(key)) {
+            map.remove(key);
+        }
+    }
+
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return map.containsKey(key);
+    }
+}
