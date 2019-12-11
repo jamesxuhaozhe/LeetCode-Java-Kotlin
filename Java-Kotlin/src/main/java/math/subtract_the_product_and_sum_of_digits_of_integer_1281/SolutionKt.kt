6ae0fb551a299984a5388ba90739f6a27030de2f
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-12-11 22:27:48 +0800

    add 1281 kt solution

---
+++ Java-Kotlin/src/main/java/math/subtract_the_product_and_sum_of_digits_of_integer_1281/SolutionKt.kt
@@ -0,0 +1,18 @@
+package math.subtract_the_product_and_sum_of_digits_of_integer_1281
+
+class SolutionKt {
+
+    fun subtractProductAndSum(n: Int): Int {
+        var sum = 0
+        var product = 1
+        var n = n
+        while(n != 0) {
+            val digit = n % 10
+            sum += digit
+            product *= digit
+            n = n / 10
+        }
+
+        return product - sum
+    }
+}
