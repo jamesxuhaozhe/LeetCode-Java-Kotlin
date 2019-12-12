86d24a806522a8c301c434361edc0d89f561ed00
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-12-12 09:30:25 +0800

    Add time and space complexity in 1281

--- Java-Kotlin/src/main/java/math/subtract_the_product_and_sum_of_digits_of_integer_1281/Solution.java
+++ Java-Kotlin/src/main/java/math/subtract_the_product_and_sum_of_digits_of_integer_1281/Solution.java
@@ -1,5 +1,10 @@
 package math.subtract_the_product_and_sum_of_digits_of_integer_1281;
 
+/**
+ * Time complexity: O(digitlen(n))
+ *
+ * Space complexity: O(1)
+ */
 public class Solution {
 
     public int subtractProductAndSum(int n) {
