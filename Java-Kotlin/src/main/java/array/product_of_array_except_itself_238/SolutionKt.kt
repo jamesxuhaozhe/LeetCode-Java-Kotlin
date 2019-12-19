d2a4271460aa0a9111c4ebfb294188cfb367a393
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-12-19 22:53:19 +0800

    Add 238 kt solution

---
+++ Java-Kotlin/src/main/java/array/product_of_array_except_itself_238/SolutionKt.kt
@@ -0,0 +1,20 @@
+package array.product_of_array_except_itself_238
+
+class SolutionKt {
+
+    fun productExceptSelf(nums: IntArray): IntArray {
+        val result = IntArray(nums.size)
+        result[0] = 1
+        for (i in 1 until nums.size) {
+            result[i] = result[i - 1] * nums[i - 1]
+        }
+
+        var right = 1
+        for (i in (nums.size - 1) downTo 0) {
+            result[i] *= right
+            right *= nums[i]
+        }
+
+        return result
+    }
+}
