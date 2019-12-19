58fe6b5d862591a799a455b7ce09bce0b6015e93
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-12-19 22:00:51 +0800

    Add problem 138

---
+++ Java-Kotlin/src/main/java/linkedList/copy_list_with_random_pointer_138/Solution.java
@@ -0,0 +1,45 @@
+package linkedList.copy_list_with_random_pointer_138;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Problem link: https://leetcode-cn.com/problems/copy-list-with-random-pointer/
+ *
+ * Time complexity: O(n), n is the length of the list
+ *
+ * Space complexity: O(n), n is the length of the list
+ */
+public class Solution {
+
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> map = new HashMap<>();
+        Node node = head;
+
+        while (node != null) {
+            map.put(node, new Node(node.val));
+            node = node.next;
+        }
+
+        node = head;
+        while (node != null) {
+            map.get(node).next = map.get(node.next);
+            map.get(node).random = map.get(node.random);
+            node = node.next;
+        }
+
+        return map.get(head);
+    }
+
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+}
