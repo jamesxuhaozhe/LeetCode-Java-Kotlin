9d06e6df30931ffbe1dfac414d0d3df0203894e4
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-12-20 16:00:09 +0800

    Add kt 1232

---
+++ Java-Kotlin/src/main/java/math/check_straight_line_1232/SolutionKt.kt
@@ -0,0 +1,25 @@
+package math.check_straight_line_1232
+
+class SolutionKt {
+
+    fun checkStraightLine(coordinates: Array<IntArray>): Boolean {
+        val initialPoint = coordinates[0]
+        val secondPoint = coordinates[1]
+
+        val deltaX = secondPoint[0] - initialPoint[0]
+        val deltaY = secondPoint[1] - initialPoint[1]
+
+        for (index in 2 until coordinates.size) {
+            val currentPoint = coordinates[index]
+
+            val x = currentPoint[0] - initialPoint[0]
+            val y = currentPoint[1] - initialPoint[1]
+
+            if (deltaX * y != deltaY * x) {
+                return false
+            }
+        }
+
+        return true
+    }
+}
