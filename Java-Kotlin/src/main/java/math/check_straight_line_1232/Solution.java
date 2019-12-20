a24fb65bea127f29fa3cefb9525003c99191d588
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-12-20 15:55:48 +0800

    Add problem 1232

---
+++ Java-Kotlin/src/main/java/math/check_straight_line_1232/Solution.java
@@ -0,0 +1,25 @@
+package math.check_straight_line_1232;
+
+public class Solution {
+
+    public boolean checkStraightLine(int[][] coordinates) {
+        int[] initialPoint = coordinates[0];
+        int[] secondPoint = coordinates[1];
+
+        int deltaX = secondPoint[0] - initialPoint[0];
+        int deltaY = secondPoint[1] - initialPoint[1];
+
+        for (int i = 2; i < coordinates.length; i++) {
+            int[] currentPoint = coordinates[i];
+
+            int x = currentPoint[0] - initialPoint[0];
+            int y = currentPoint[1] - initialPoint[1];
+
+            if (deltaX * y != deltaY * x) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
