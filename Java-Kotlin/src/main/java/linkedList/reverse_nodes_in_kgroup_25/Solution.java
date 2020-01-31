5e23fcd075fa4d866521641195e6f15157717d04
James Xu <jamesxuhaozhe@gmail.com>
2020-01-31 22:58:44 +0800

    Add java solution for problem 25

---
+++ Java-Kotlin/src/main/java/linkedList/reverse_nodes_in_kgroup_25/Solution.java
@@ -0,0 +1,42 @@
+package linkedList.reverse_nodes_in_kgroup_25;
+
+import datastructure.ListNode;
+
+public class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode end = dummy;
+
+        while (end != null) {
+            for (int i = 0; i < k && end != null; i++) {
+                end = end.next;
+            }
+            if (end == null) {
+                break;
+            }
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+            end = pre;
+        }
+
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+}
