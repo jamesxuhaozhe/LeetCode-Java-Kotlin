28da8dbf03db409bae506b62a47653c42d6c03bb
James Xu <jamesxuhaozhe@gmail.com>
2020-02-01 21:47:19 +0800

    add Java and kt solution for problem 92

--- Java-Kotlin/src/main/java/datastructure/ListNode.java
+++ Java-Kotlin/src/main/java/datastructure/ListNode.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import java.util.List;
+
 public class ListNode {
     public int val;
     public ListNode next;
@@ -16,5 +18,30 @@ public class ListNode {
         }
 
         return sb.toString();
+    }
+
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        System.out.println();
+        while (temp != null) {
+            System.out.print(temp.val + "\t");
+            temp = temp.next;
+        }
+    }
+
+    public static ListNode createSinglyLinkedList(List<Integer> listValues) {
+        if (listValues == null || listValues.size() == 0) {
+            throw new IllegalArgumentException(
+                    "Please pass in a valid listValues to create a singly linked list.");
+        }
+        ListNode head = new ListNode(listValues.get(0));
+        ListNode tmp = head;
+        for (int i = 1; i < listValues.size(); i++) {
+            ListNode next = new ListNode(listValues.get(i));
+            tmp.next = next;
+            tmp = tmp.next;
+        }
+        printList(head);
+        return head;
     }
 }
