cf2702bc35b206c193d734c1f8d27a96959ddb1e
James Xu <jamesxuhaozhe@gmail.com>
2020-02-02 23:04:18 +0800

    add go and java solution for problem 328

---
+++ Go-Solutions/328/328.go
@@ -0,0 +1,27 @@
+package main
+
+import "Go-Solutions/ds"
+
+func main() {
+	
+}
+
+func oddEvenList(head *ds.ListNode) *ds.ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	odd := head
+	even := odd.Next
+	evenHead := even
+
+	for even != nil && even.Next != nil {
+		odd.Next = even.Next
+		odd = odd.Next
+
+		even.Next = odd.Next
+		even = even.Next
+	}
+	odd.Next = evenHead
+	return head
+}
