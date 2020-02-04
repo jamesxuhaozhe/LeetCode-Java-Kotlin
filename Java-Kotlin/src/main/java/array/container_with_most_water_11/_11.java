b9ef649efad07ecc8695d11fd9d85945c2d554ca
James Xu <jamesxuhaozhe@gmail.com>
2020-02-04 17:14:34 +0800

    add java solution for problem 11

---
+++ Java-Kotlin/src/main/java/array/container_with_most_water_11/_11.java
@@ -0,0 +1,19 @@
+package array.container_with_most_water_11;
+
+public class _11 {
+
+    public int maxArea(int[] height) {
+        int maxArea = Integer.MIN_VALUE;
+        int i = 0;
+        int j = height.length - 1;
+        while (i < j) {
+            maxArea = Math.max(maxArea, Math.min(height[i], height[j]) * (j - i));
+            if (height[i] <= height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return maxArea;
+    }
+}
