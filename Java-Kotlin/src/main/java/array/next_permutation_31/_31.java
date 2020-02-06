cc63f6a739717f430ba1a72805d11218c49a8ea0
James Xu <jamesxuhaozhe@gmail.com>
2020-02-06 14:29:47 +0800

    add java solution for problem 31 34 33

---
+++ Java-Kotlin/src/main/java/array/next_permutation_31/_31.java
@@ -0,0 +1,39 @@
+package array.next_permutation_31;
+
+public class _31 {
+
+    public void nextPermutation(int[] nums) {
+        // if the len of nums is 0 or 1 then just return
+        if (nums == null || nums.length <= 1) {
+            return;
+        }
+
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int end = nums.length - 1;
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = tmp;
+    }
+}
