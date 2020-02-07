6767a19f24efbb19dae5ff12dbbe7cf44f59c603
James Xu <jamesxuhaozhe@gmail.com>
2020-02-07 20:32:21 +0800

    add java solution for problem  39

---
+++ Java-Kotlin/src/main/java/backtrace/_39.java
@@ -0,0 +1,42 @@
+package backtrace;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _39 {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (candidates == null || candidates.length == 0 || target < 0) {
+            return result;
+        }
+
+        Arrays.sort(candidates);
+        List<Integer> list = new ArrayList<>();
+        helper(0, candidates, result, target, list);
+        return result;
+    }
+
+    private void helper(int start, int[] candidates, List<List<Integer>> result, int target, List<Integer> list) {
+        if (target < 0) {
+            return;
+        }
+
+        if (target == 0) {
+            result.add(new ArrayList<>(list));
+        } else {
+            for (int i = start; i < candidates.length; i++) {
+                list.add(candidates[i]);
+                helper(i, candidates, result, target - candidates[i], list);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {2, 3, 6, 7};
+        int target = 7;
+        System.out.println(new _39().combinationSum(nums, target));
+    }
+}
