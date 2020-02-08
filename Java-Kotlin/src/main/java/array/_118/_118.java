b91231fe2117a1f62ad0a9ad9fa770a0a6665281
James Xu <jamesxuhaozhe@gmail.com>
2020-02-08 16:14:39 +0800

    add java solution for problem 118

---
+++ Java-Kotlin/src/main/java/array/_118/_118.java
@@ -0,0 +1,31 @@
+package array._118;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _118 {
+    public List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        for (int currRow = 0; currRow < numRows; currRow++) {
+            List<Integer> rowList = getInitialRowList(currRow + 1);
+            for (int j = 0; j < rowList.size(); j++) {
+                if (currRow > 1 && (j > 0 && j < rowList.size() - 1)) {
+                    List<Integer> preList = result.get(currRow - 1);
+                    rowList.set(j, preList.get(j - 1) + preList.get(j));
+                }
+            }
+            result.add(rowList);
+        }
+
+        return result;
+    }
+
+    private List<Integer> getInitialRowList(int capacity) {
+        List<Integer> result = new ArrayList<>(capacity);
+        for (int i = 0; i < capacity; i++) {
+            result.add(1);
+        }
+        return result;
+    }
+}
