cbb33503b384af3177d9daead1f3273bc1f91418
James Xu <jamesxuhaozhe@gmail.com>
2020-02-10 21:54:36 +0800

    add java solutoin for problem 235 & 236

---
+++ Java-Kotlin/src/main/java/tree/_235.java
@@ -0,0 +1,18 @@
+package tree;
+
+import common.datastructure.TreeNode;
+
+public class _235 {
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+
+        return root;
+    }
+}
