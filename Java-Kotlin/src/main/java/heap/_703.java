2863cebecc2f851f456aee31126ca82b526df8b9
James Xu <jamesxuhaozhe@gmail.com>
2020-02-10 20:04:21 +0800

    Add java solution for problem 703

---
+++ Java-Kotlin/src/main/java/heap/_703.java
@@ -0,0 +1,28 @@
+package heap;
+
+import java.util.PriorityQueue;
+
+public class _703 {
+
+    private PriorityQueue<Integer> priorityQueue;
+
+    private int k;
+
+    public _703(int k, int[] nums) {
+        this.k = k;
+        this.priorityQueue = new PriorityQueue<>(k);
+        for (int i : nums) {
+            add(i);
+        }
+    }
+
+    public int add(int val) {
+        if (priorityQueue.size() < k) {
+            priorityQueue.offer(val);
+        } else if (priorityQueue.peek() < val){
+            priorityQueue.poll();
+            priorityQueue.offer(val);
+        }
+        return priorityQueue.peek();
+    }
+}
