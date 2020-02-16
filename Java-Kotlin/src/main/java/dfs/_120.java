f2e776c1204b8c41e648835a34267c4362818497
James Xu <jamesxuhaozhe@gmail.com>
2020-02-16 17:25:34 +0800

    Add another way to solve 120

--- Java-Kotlin/src/main/java/dfs/_120.java
+++ Java-Kotlin/src/main/java/dfs/_120.java
@@ -20,4 +20,26 @@ public class _120 {
         }
         return dp.get(0);
     }
+
+    public int minimumTotal1(List<List<Integer>> triangle) {
+        if (triangle == null || triangle.isEmpty()) {
+            return 0;
+        }
+
+        int m = triangle.size();
+        int n = triangle.get(m - 1).size();
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < triangle.get(m - 1).size(); i++) {
+            dp[m - 1][i] = triangle.get(m - 1).get(i);
+        }
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i + 1][j], dp[i + 1][j + 1]);
+            }
+        }
+
+        return dp[0][0];
+    }
+
 }
