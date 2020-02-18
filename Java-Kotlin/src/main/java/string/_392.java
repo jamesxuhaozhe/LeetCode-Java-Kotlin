4605d9944a5a48a46cef8d094ae5dde3bae35ea3
James Xu <jamesxuhaozhe@gmail.com>
2020-02-18 22:25:25 +0800

    392

---
+++ Java-Kotlin/src/main/java/string/_392.java
@@ -0,0 +1,44 @@
+package string;
+
+public class _392 {
+
+    //leverage the Java library string.indexOf(ch, int), it will start from index int, and look for the first ch, if
+    // can't find it, then return -1
+    public boolean isSubsequence(String s, String t) {
+        char[] chars = s.toCharArray();
+        int j = -1;
+        for (int i = 0; i < chars.length; i++) {
+            j = t.indexOf(chars[i], j + 1);
+            if (j == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Double pointer, one point to the starting index of s, the other pointing to the starting index of t
+     * if there is no match, we move j forward until we have a match, once we have a match, we move them both by one step.
+     * if s is the subsequence of t, then i should be equal to len(s)
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isSubsequence1(String s, String t) {
+        int i = 0;
+        int j = 0;
+        int sLen = s.length();
+        int tLen = t.length();
+        while (i < sLen && j < tLen) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return i == sLen;
+    }
+
+
+}
