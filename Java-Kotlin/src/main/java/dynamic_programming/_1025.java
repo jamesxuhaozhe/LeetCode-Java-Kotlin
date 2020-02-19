94d65f21c9c12149978ea34b68eed132ac0afbf4
James Xu <jamesxuhaozhe@gmail.com>
2020-02-19 21:26:27 +0800

    Add problem 1025

---
+++ Java-Kotlin/src/main/java/dynamic_programming/_1025.java
@@ -0,0 +1,22 @@
+package dynamic_programming;
+
+public class _1025 {
+
+    /**
+     * 基本思路：
+     *
+     * 最终结果应该是占到 2 的赢，占到 1 的输；
+     *
+     * 若当前为奇数，奇数的约数只能是奇数或者 1，因此下一个一定是偶数；
+     *
+     * 若当前为偶数， 偶数的约数可以是奇数可以是偶数也可以是 1，因此直接减 1，则下一个是奇数；
+     *
+     * 因此，奇则输，偶则赢。直接:
+     *
+     * @param N
+     * @return
+     */
+    public boolean divisorGame(int N) {
+        return (N & 1) != 1;
+    }
+}
