95b05799518f2265852c4deb8460226186fb4294
James Xu <jamesxuhaozhe@gmail.com>
2020-02-20 22:43:46 +0800

    add problem 03

---
+++ Java-Kotlin/src/main/java/array/_03.java
@@ -0,0 +1,19 @@
+package array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class _03 {
+
+    public int findRepeatNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int repeat = -1;
+        for(int num : nums) {
+            if (!set.add(num)) {
+                repeat = num;
+                break;
+            }
+        }
+        return repeat;
+    }
+}
