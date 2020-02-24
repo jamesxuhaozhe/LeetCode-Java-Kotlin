ae645f4b88383aee64bff4387cbf94a80ffc99e5
James Xu <jamesxuhaozhe@gmail.com>
2020-02-24 23:06:46 +0800

    Replace with lambda

--- Java-Kotlin/src/main/java/sort/largest_number_179/Solution.java
+++ Java-Kotlin/src/main/java/sort/largest_number_179/Solution.java
@@ -18,13 +18,10 @@ public class Solution {
             numStr[i] = String.valueOf(nums[i]);
         }
 
-        Arrays.sort(numStr, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                String order1 = o1 + o2;
-                String order2 = o2 + o1;
-                return order2.compareTo(order1);
-            }
+        Arrays.sort(numStr, (o1, o2) -> {
+            String order1 = o1 + o2;
+            String order2 = o2 + o1;
+            return order2.compareTo(order1);
         });
 
         if (numStr.length != 0 && numStr[0].equals("0")) {
