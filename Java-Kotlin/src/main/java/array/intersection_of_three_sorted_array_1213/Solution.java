9304a4af7b18e6886fa7a8fdf4ba5bdb798f8ff9
James Xu <jamesxuhaozhe@gmail.com>
2020-03-03 22:52:19 +0800

    Clean up the imports

--- Java-Kotlin/src/main/java/array/intersection_of_three_sorted_array_1213/Solution.java
+++ Java-Kotlin/src/main/java/array/intersection_of_three_sorted_array_1213/Solution.java
@@ -1,6 +1,10 @@
 package array.intersection_of_three_sorted_array_1213;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Solution {
 
