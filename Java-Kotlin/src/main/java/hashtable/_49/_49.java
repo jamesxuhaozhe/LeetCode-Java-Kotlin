5b0062aa7dcd3af6983dafeded35c5f61da7f71c
James Xu <jamesxuhaozhe@gmail.com>
2020-03-08 22:27:11 +0800

    49 problem

---
+++ Java-Kotlin/src/main/java/hashtable/_49/_49.java
@@ -0,0 +1,65 @@
+package hashtable._49;
+
+import java.util.*;
+
+public class _49 {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return Collections.emptyList();
+        }
+
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            String basicOrderStr = getBasicOrderStr(str);
+            if (map.containsKey(basicOrderStr)) {
+                map.get(basicOrderStr).add(str);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(basicOrderStr, list);
+            }
+        }
+
+        List<List<String >> result = new ArrayList<>();
+        result.addAll(map.values());
+        return result;
+    }
+
+    private static String getBasicOrderStr(String str) {
+        if (str.equals("")) {
+            return "empty";
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            pq.add((int) chars[i]);
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!pq.isEmpty()) {
+            sb.append(pq.poll());
+        }
+
+        return sb.toString();
+    }
+
+
+    public List<List<String>> groupAnagrams1(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return Collections.emptyList();
+        }
+
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] ca = str.toCharArray();
+            Arrays.sort(ca);
+            String basicOrderStr = String.valueOf(ca);
+            if (!map.containsKey(basicOrderStr)) {
+                map.put(basicOrderStr, new ArrayList<>());
+            }
+            map.get(basicOrderStr).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
