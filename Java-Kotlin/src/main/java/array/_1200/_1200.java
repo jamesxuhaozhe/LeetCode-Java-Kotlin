c75853e15a52f4c052b8ce21d0a943d3684e41cc
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-03-10 09:33:10 +0800

    Add problem 1200

---
+++ Java-Kotlin/src/main/java/array/_1200/_1200.java
@@ -0,0 +1,32 @@
+package array._1200;
+
+import java.util.*;
+
+public class _1200 {
+
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        // first sort the array
+        Arrays.sort(arr);
+        int minDiff = Integer.MAX_VALUE;
+        int lastValue = arr[0];
+        // 用一个map来记录，每两两之间的绝对差值，key是绝对差值，value是具有该绝对差值的两两对。
+        Map<Integer, List<List<Integer>>> map = new HashMap<>();
+        for (int i = 1; i < arr.length; i++) {
+            int currentDiff = Math.abs(lastValue - arr[i]);
+            if (map.containsKey(currentDiff)) {
+                map.get(currentDiff).add(Arrays.asList(lastValue, arr[i]));
+            } else {
+                List<List<Integer>> list = new ArrayList<>();
+                list.add(Arrays.asList(lastValue, arr[i]));
+                map.put(currentDiff, list);
+            }
+            //找到最小的绝对差值
+            minDiff = Math.min(minDiff, Math.abs(lastValue - arr[i]));
+
+            lastValue = arr[i];
+        }
+
+        return map.get(minDiff);
+    }
+
+}
