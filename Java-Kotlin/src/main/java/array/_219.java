946a477d2ae6dae78361335b3c2a8e486c45b362
James Xu <jamesxuhaozhe@gmail.com>
2020-03-13 21:46:07 +0800

    Add problem 219

---
+++ Java-Kotlin/src/main/java/array/_219.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _219 {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if (Math.abs(i - map.get(nums[i])) < k) {
+                    return true;
+                } else {
+                    map.put(nums[i], i);
+                }
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+
+        return false;
+    }
+}
