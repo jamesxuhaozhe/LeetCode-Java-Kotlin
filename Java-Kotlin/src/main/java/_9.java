5e998cda82c3982b0d7b82d54625fd4341b1ce23
James Xu <jamesxuhaozhe@gmail.com>
2020-03-13 21:50:08 +0800

    add problem 9

---
+++ Java-Kotlin/src/main/java/_9.java
@@ -0,0 +1,16 @@
+public class _9 {
+
+    public boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+        int left = 0;
+        int right = str.length() - 1;
+        while(left <= right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
