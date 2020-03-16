1e9ddf1c9a107547efb4e20e3344d17329ad5579
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-03-16 12:25:44 +0800

    Add problem 215

---
+++ Java-Kotlin/src/main/java/array/_215.java
@@ -0,0 +1,35 @@
+package array;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class _215 {
+
+    /**
+     * This is actually faster
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        int[] n = new int[nums.length];
+        int index = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            n[index++] = nums[i];
+        }
+
+        return n[k - 1];
+    }
+
+    public int findKthLargest1(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num : nums) {
+            pq.offer(num);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        return pq.poll();
+    }
+}
