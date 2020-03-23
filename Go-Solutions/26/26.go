9618c058e75552214a0f06ef692402c9771f8bdf
James Xu <jamesxuhaozhe@gmail.com>
2020-03-23 22:38:50 +0800

    Add gol sol for problem 26

---
+++ Go-Solutions/26/26.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+	
+}
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		if nums[i] != nums[j] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+
+	return i + 1
+}
