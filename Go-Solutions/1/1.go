47645673cc62e90005ea3bd234805fa061d6da7a
James Xu <jamesxuhaozhe@gmail.com>
2020-03-23 22:17:49 +0800

    Add go solution for problem 1

---
+++ Go-Solutions/1/1.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	
+}
+
+func twoSum(nums []int, target int) []int {
+	helperMap := make(map[int]int)
+	for i, v := range nums {
+		complement := target - v
+		if j, ok := helperMap[complement]; ok && (j != i) {
+			return []int{i, j}
+		}
+		helperMap[v] = i
+	}
+	panic("No solution")
+}
