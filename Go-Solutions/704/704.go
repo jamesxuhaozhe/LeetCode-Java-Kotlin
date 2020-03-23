c284cfb14a1245fc81a265e855d8f557f33ff2d7
James Xu <jamesxuhaozhe@gmail.com>
2020-03-23 22:33:59 +0800

    Add go sol for problem 704

---
+++ Go-Solutions/704/704.go
@@ -0,0 +1,28 @@
+package main
+
+func main() {
+	
+}
+
+func search(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+
+	start := 0
+	end := len(nums) - 1
+	for start <= end {
+		mid := (start + end) / 2
+		if target == nums[mid] {
+			return mid
+		}
+		if target < nums[mid] {
+			end = mid - 1
+		}
+		if target > nums[mid] {
+			start = mid + 1
+		}
+	}
+
+	return -1
+}
