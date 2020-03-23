56379b6a0a437b1c35a3ba28ff4dc58a461ec417
James Xu <jamesxuhaozhe@gmail.com>
2020-03-23 22:28:43 +0800

    Add go sol for problem 167

---
+++ Go-Solutions/167/167.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+	
+}
+
+func twoSum(numbers []int, target int) []int {
+	start := 0
+	last := len(numbers) - 1
+	for start < last {
+		sum := numbers[start] + numbers[last]
+		if sum == target {
+			return []int{start + 1, last + 1}
+		}
+		if sum < target {
+			start++
+		} else {
+			last--
+		}
+	}
+
+	return nil
+}
