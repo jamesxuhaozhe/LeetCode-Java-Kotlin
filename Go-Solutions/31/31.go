b29067c515db8525f5307a5528637218d1ec21a2
James Xu <jamesxuhaozhe@gmail.com>
2020-03-23 23:05:31 +0800

    Add go sol for problem 31

---
+++ Go-Solutions/31/31.go
@@ -0,0 +1,39 @@
+package main
+
+func main() {
+	
+}
+
+func nextPermutation(nums []int)  {
+	if len(nums) <= 1 {
+		return
+	}
+
+	i := len(nums) - 2
+	for i >= 0 && nums[i] >= nums[i + 1] {
+		i--
+	}
+	if i >= 0 {
+		j := len(nums) - 1
+		for j >= 0 && nums[j] <= nums[i] {
+			j--
+		}
+		swap(nums, i, j)
+	}
+	reverse(nums, i + 1)
+}
+
+func reverse(nums []int, start int) {
+	end := len(nums) - 1
+	for start < end {
+		swap(nums, start, end)
+		start++
+		end--
+	}
+}
+
+func swap(nums []int, i int, j int) {
+	tmp := nums[j]
+	nums[j] = nums[i]
+	nums[i] = tmp
+}
