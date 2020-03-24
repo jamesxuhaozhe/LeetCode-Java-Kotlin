5b344c9346a4a2b4463b688f37c035be56b2bdc4
James Xu <jamesxuhaozhe@gmail.com>
2020-03-24 23:03:18 +0800

    Add go sol for problem 977

---
+++ Go-Solutions/977/977.go
@@ -0,0 +1,17 @@
+package main
+
+import "sort"
+
+func main() {
+	
+}
+
+func sortedSquares(A []int) []int {
+	n := len(A)
+	result := make([]int, n, n)
+	for i := 0; i < n; i++ {
+		result[i] = A[i] * A[i]
+	}
+	sort.Ints(result)
+	return result
+}
