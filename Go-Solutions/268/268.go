985072d8afcf14be49dcc383f21b2b7fe2ca8b13
James Xu <jamesxuhaozhe@gmail.com>
2020-03-24 23:06:44 +0800

    Add gol sol for problem 268

---
+++ Go-Solutions/268/268.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	
+}
+
+func missingNumber(nums []int) int {
+	n := len(nums)
+	expectedSum := n * (n + 1) / 2
+
+	actualSum := 0
+	for _, v := range nums {
+		actualSum += v
+	}
+
+	return expectedSum - actualSum
+}
