ba84e36ba213c1d5c54daa3e67485d993f03437b
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-05-02 13:57:04 +0800

    Change receiver variable name

--- Go-Solutions/707.go
+++ Go-Solutions/707.go
@@ -30,50 +30,50 @@ func Constructor() MyLinkedList {
 }
 
 /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
-func (this *MyLinkedList) Get(index int) int {
-	if index >= 0 && index < this.size {
-		return this.getNodeAtIndex(index).val
+func (m *MyLinkedList) Get(index int) int {
+	if index >= 0 && index < m.size {
+		return m.getNodeAtIndex(index).val
 	}
 	return -1
 }
 
 /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
-func (this *MyLinkedList) AddAtHead(val int) {
-	preHead := this.headDummy.next
+func (m *MyLinkedList) AddAtHead(val int) {
+	preHead := m.headDummy.next
 	currHead := &myDoublyLinkedList{
 		val: val,
 	}
-	this.headDummy.next = currHead
-	currHead.previous = this.headDummy
+	m.headDummy.next = currHead
+	currHead.previous = m.headDummy
 	currHead.next = preHead
 	preHead.previous = currHead
-	this.size++
+	m.size++
 }
 
 /** Append a node of value val to the last element of the linked list. */
-func (this *MyLinkedList) AddAtTail(val int) {
-	preTail := this.tailDummy.previous
+func (m *MyLinkedList) AddAtTail(val int) {
+	preTail := m.tailDummy.previous
 	currTail := &myDoublyLinkedList{
 		val: val,
 	}
 	preTail.next = currTail
 	currTail.previous = preTail
-	currTail.next = this.tailDummy
-	this.tailDummy.previous = currTail
-	this.size++
+	currTail.next = m.tailDummy
+	m.tailDummy.previous = currTail
+	m.size++
 }
 
 /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
-func (this *MyLinkedList) AddAtIndex(index int, val int) {
-	if index > this.size {
+func (m *MyLinkedList) AddAtIndex(index int, val int) {
+	if index > m.size {
 		return
 	}
 
-	if index == this.size {
-		this.AddAtTail(val)
+	if index == m.size {
+		m.AddAtTail(val)
 	}
 
-	nodeAtIndex := this.getNodeAtIndex(index)
+	nodeAtIndex := m.getNodeAtIndex(index)
 	preNodeAtIndex := nodeAtIndex.previous
 	nodeToAdd := &myDoublyLinkedList{
 		val: val,
@@ -82,11 +82,11 @@ func (this *MyLinkedList) AddAtIndex(index int, val int) {
 	nodeToAdd.previous = preNodeAtIndex
 	nodeToAdd.next = nodeAtIndex
 	nodeAtIndex.previous = nodeToAdd
-	this.size++
+	m.size++
 }
 
-func (this *MyLinkedList) getNodeAtIndex(index int) *myDoublyLinkedList {
-	currHead := this.headDummy.next
+func (m *MyLinkedList) getNodeAtIndex(index int) *myDoublyLinkedList {
+	currHead := m.headDummy.next
 	for i := 0; i < index; i++ {
 		currHead = currHead.next
 	}
@@ -94,13 +94,13 @@ func (this *MyLinkedList) getNodeAtIndex(index int) *myDoublyLinkedList {
 }
 
 /** Delete the index-th node in the linked list, if the index is valid. */
-func (this *MyLinkedList) DeleteAtIndex(index int) {
-	if index >= 0 && index < this.size {
-		nodeAtIndex := this.getNodeAtIndex(index)
+func (m *MyLinkedList) DeleteAtIndex(index int) {
+	if index >= 0 && index < m.size {
+		nodeAtIndex := m.getNodeAtIndex(index)
 		preNodeAtIndex := nodeAtIndex.previous
 		nextNodeAtIndex := nodeAtIndex.next
 		preNodeAtIndex.next = nextNodeAtIndex
 		nextNodeAtIndex.previous = preNodeAtIndex
-		this.size--
+		m.size--
 	}
 }
