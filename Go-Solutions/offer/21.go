99d6d844219e2dc83aa023f326061a3a41cf3603
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-07-10 22:29:17 +0800

    format code

--- Go-Solutions/offer/21.go
+++ Go-Solutions/offer/21.go
@@ -9,7 +9,7 @@ func exchange(nums []int) []int {
 	left := 0
 	right := n - 1
 	for i := 0; i < n; i++ {
-		if nums[i] % 2 == 1 {
+		if nums[i]%2 == 1 {
 			result[left] = nums[i]
 			left++
 		} else {
--- Java-Kotlin/src/main/java/stack/_225.java
+++ Java-Kotlin/src/main/java/stack/_225.java
@@ -5,10 +5,10 @@ import java.util.Queue;
 
 /**
  * https://leetcode-cn.com/problems/implement-stack-using-queues/
- *
+ * <p>
  * Solution 1: Use two queues, and use a variable to keep track of the top value. when poping, you need to move
  * q1.size - 1 numbers of elements to q2
- *
+ * <p>
  * Solution2: just use one queue.
  */
 public class _225 {
@@ -23,20 +23,26 @@ public class _225 {
 
             private int top;
 
-            /** Initialize your data structure here. */
+            /**
+             * Initialize your data structure here.
+             */
             public MyStack() {
                 q1 = new LinkedList<>();
                 q2 = new LinkedList<>();
                 top = 0;
             }
 
-            /** Push element x onto stack. */
+            /**
+             * Push element x onto stack.
+             */
             public void push(int x) {
                 q1.add(x);
                 top = x;
             }
 
-            /** Removes the element on top of the stack and returns that element. */
+            /**
+             * Removes the element on top of the stack and returns that element.
+             */
             public int pop() {
                 while (q1.size() > 1) {
                     top = q1.remove();
@@ -49,12 +55,16 @@ public class _225 {
                 return pop;
             }
 
-            /** Get the top element. */
+            /**
+             * Get the top element.
+             */
             public int top() {
                 return top;
             }
 
-            /** Returns whether the stack is empty. */
+            /**
+             * Returns whether the stack is empty.
+             */
             public boolean empty() {
                 return q1.isEmpty();
             }
@@ -66,12 +76,16 @@ public class _225 {
 
             private Queue<Integer> q1;
 
-            /** Initialize your data structure here. */
+            /**
+             * Initialize your data structure here.
+             */
             public MyStack() {
                 q1 = new LinkedList<>();
             }
 
-            /** Push element x onto stack. */
+            /**
+             * Push element x onto stack.
+             */
             public void push(int x) {
                 q1.add(x);
                 int size = q1.size();
@@ -80,17 +94,23 @@ public class _225 {
                 }
             }
 
-            /** Removes the element on top of the stack and returns that element. */
+            /**
+             * Removes the element on top of the stack and returns that element.
+             */
             public int pop() {
                 return q1.poll();
             }
 
-            /** Get the top element. */
+            /**
+             * Get the top element.
+             */
             public int top() {
                 return q1.peek();
             }
 
-            /** Returns whether the stack is empty. */
+            /**
+             * Returns whether the stack is empty.
+             */
             public boolean empty() {
                 return q1.isEmpty();
             }
