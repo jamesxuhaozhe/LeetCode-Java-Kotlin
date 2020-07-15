af0ed535a5c75a8df708eca09a27b1a69b84ef53
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-07-15 16:02:53 +0800

    Add problem java 226

---
+++ Java-Kotlin/src/main/java/tree/_226/_226.java
@@ -0,0 +1,27 @@
+package tree._226;
+
+import common.datastructure.TreeNode;
+
+/**
+ * Problem link: https://leetcode-cn.com/problems/invert-binary-tree/submissions/
+ *
+ * Solution: just post order traversal
+ */
+public class _226 {
+
+    public TreeNode invertTree(TreeNode root) {
+        helper(root);
+        return root;
+    }
+
+    private void helper(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        helper(root.left);
+        helper(root.right);
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+    }
+}
