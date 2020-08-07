ddde50f0aaebc106a1e58b04b31afb543c133756
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-08-07 14:03:32 +0800

    add go solution for 100

---
+++ Go-Solutions/100.go
@@ -0,0 +1,17 @@
+package main
+
+import ds "Go-Solutions/ds"
+
+func isSameTree(p *ds.TreeNode, q *ds.TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+
+	return isSameTree(p.Right, q.Right) && isSameTree(p.Left, q.Left)
+}
