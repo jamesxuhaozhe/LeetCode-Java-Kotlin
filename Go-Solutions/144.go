dfa4d6131d12dc3ddac3f9bbcb8a2103e8581d0e
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-09-16 16:15:51 +0800

    add iterative solution for problem 144

--- Go-Solutions/144.go
+++ Go-Solutions/144.go
@@ -5,7 +5,7 @@ import "Go-Solutions/ds"
 /**
 Problem link: https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
  */
-func preorderTraversal(root *ds.TreeNode) []int {
+func preorderTraversal1(root *ds.TreeNode) []int {
 	var result []int
 	preOrder(root, &result)
 	return result
@@ -18,4 +18,30 @@ func preOrder(root *ds.TreeNode, result *[]int) {
 	*result = append(*result, root.Val)
 	preOrder(root.Left, result)
 	preOrder(root.Right, result)
+}
+
+// iterative
+func preorderTraversal2(root *ds.TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+
+	stack, result := []*ds.TreeNode{}, []int{}
+	stack = append(stack, root)
+	for len(stack) != 0 {
+		// stack 的实现方式
+		node := stack[len(stack) - 1]
+		// golang 里面把栈顶的元素出栈
+		stack = stack[:len(stack) - 1]
+		if node != nil {
+			result = append(result, node.Val)
+		}
+		if node != nil && node.Right != nil {
+			stack = append(stack, node.Right)
+		}
+		if node != nil && node.Left != nil {
+			stack = append(stack, node.Left)
+		}
+	}
+	return result
 }
