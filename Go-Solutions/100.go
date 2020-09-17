4f29994383d91a21a099d8463b27ba34b43a0ec7
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-09-17 12:49:26 +0800

    some more comments

--- Go-Solutions/100.go
+++ Go-Solutions/100.go
@@ -1,6 +1,7 @@
 package main
 
 // just do recursion
+// Problem link: https://leetcode-cn.com/problems/same-tree/
 func isSameTree(p *TreeNode, q *TreeNode) bool {
 	if p == nil && q == nil {
 		return true
