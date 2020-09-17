8b6a1bd7e70d7c34dc74ad8ac98233f953ba4012
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-09-17 15:55:35 +0800

    Add problem 108

--- Go-Solutions/108.go
+++ Go-Solutions/108.go
@@ -13,3 +13,25 @@ func sortedArrayToBST(nums []int) *TreeNode {
 		Right: sortedArrayToBST(nums[len(nums)/2+1:]),
 	}
 }
+
+
+func sortedArrayToBST1(nums []int) *TreeNode {
+	if nums == nil || len(nums) == 0 {
+		return nil
+	}
+
+	return createBST108(nums, 0, len(nums) - 1)
+}
+
+func createBST108(nums []int, start int, end int) *TreeNode {
+	if start > end {
+		return nil
+	}
+	
+	mid := (start + end) / 2
+	return &TreeNode{
+		Val:   nums[mid],
+		Left:  createBST108(nums, start, mid-1),
+		Right: createBST108(nums, mid+1, end),
+	}
+}
