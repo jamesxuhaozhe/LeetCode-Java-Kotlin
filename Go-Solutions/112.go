4993d319c9e2e1e3b559e00598baf8f7d6ecd6de
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-09-18 08:59:21 +0800

    Add problem 112

---
+++ Go-Solutions/112.go
@@ -0,0 +1,14 @@
+package main
+
+// Problem link:https://leetcode-cn.com/problems/path-sum/
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+
+	if root.Left == nil && root.Right == nil {
+		return root.Val == sum
+	}
+
+	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
+}
