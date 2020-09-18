8c759228b6a6489133ac41ad4fea63adfcd8a369
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-09-18 14:15:58 +0800

    add problem 230

---
+++ Go-Solutions/230.go
@@ -0,0 +1,28 @@
+package main
+
+// Problem link: https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/
+// inorder traversal, in a bst, if you do inorder traversal, the array you get is ascending
+func kthSmallest(root *TreeNode, k int) int {
+	if root == nil {
+		return -1
+	}
+
+	arr := make([]int, 0)
+	inorder230(root, &arr, k)
+	return arr[k - 1]
+}
+
+func inorder230(root *TreeNode, arr *[]int, k int) {
+	if root == nil {
+		return
+	}
+
+	inorder230(root.Left, arr, k)
+	*arr = append(*arr, root.Val)
+	// early return
+	if len(*arr) == k {
+		return
+	}
+	inorder230(root.Right, arr, k)
+
+}
