a07d8baade3636ab87a7eeb6f3672783e37a52dc
James Xu <jamesxuhaozhe@gmail.com>
2020-09-19 23:07:14 +0800

    Add offer 54

---
+++ Go-Solutions/jianzhi/54.go
@@ -0,0 +1,21 @@
+package jianzhi
+
+// Problem link: https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof/
+// in order traversal first
+func kthLargest54(root *TreeNode, k int) int {
+	arr := make([]int, 0)
+	inorder54(root, &arr)
+	return arr[len(arr)-k]
+}
+
+func inorder54(root *TreeNode, arr *[]int) {
+	if root == nil {
+		return
+	}
+
+	inorder54(root.Left, arr)
+
+	*arr = append(*arr, root.Val)
+
+	inorder54(root.Right, arr)
+}
