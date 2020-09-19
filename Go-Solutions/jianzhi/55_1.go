950e350d5925b41381c2ecbaa1b1a20eb3930e64
James Xu <jamesxuhaozhe@gmail.com>
2020-09-19 22:47:36 +0800

    Add more problem

---
+++ Go-Solutions/jianzhi/55_1.go
@@ -0,0 +1,17 @@
+package jianzhi
+
+// Problem link: https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/
+// nothing but standard depth of binary tree
+func maxDepth55_1(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max55_1(maxDepth55_1(root.Left), maxDepth55_1(root.Right)) + 1
+}
+
+func max55_1(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
