82025173b2495bdb358747633a583b9ad540275f
James Xu <jamesxuhaozhe@gmail.com>
2020-09-20 21:52:44 +0800

    Add offer 28

---
+++ Go-Solutions/jianzhi/28.go
@@ -0,0 +1,20 @@
+package jianzhi
+
+func isSymmetric28(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return isSymmetric28Helper(root.Left, root.Right)
+}
+
+func isSymmetric28Helper(left *TreeNode, right *TreeNode) bool {
+	if left == nil || right == nil {
+		return left == right
+	}
+
+	if left.Val != right.Val {
+		return false
+	}
+
+	return isSymmetric28Helper(left.Left, right.Right) && isSymmetric28Helper(left.Right, right.Left)
+}
