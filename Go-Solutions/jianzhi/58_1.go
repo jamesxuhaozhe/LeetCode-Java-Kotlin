06d22ebdb2b7b8f977e3178b0079303065c2c248
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-09-22 11:33:56 +0800

    Add offer 58_1

---
+++ Go-Solutions/jianzhi/58_1.go
@@ -0,0 +1,17 @@
+package jianzhi
+
+import "strings"
+
+// Problem link: https://leetcode-cn.com/problems/fan-zhuan-dan-ci-shun-xu-lcof/
+func reverseWords(s string) string {
+	res := ""
+	slice := strings.Fields(s)
+	for i := len(slice) - 1; i >= 0; i-- {
+		if i != 0 {
+			res += slice[i] + " "
+		} else {
+			res += slice[i]
+		}
+	}
+	return res
+}
