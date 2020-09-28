fd9b3b803142790ed36fdfc296ed9c0cf4406460
James Xu <jamesxuhaozhe@gmail.com>
2020-09-28 20:43:00 +0800

    Add problem 11 and 154

---
+++ Go-Solutions/154.go
@@ -0,0 +1,22 @@
+package main
+
+// Problem link: https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii/
+// same as offer 11 logn
+func findMin(nums []int) int {
+	if nums == nil || len(nums) == 0 {
+		return -1
+	}
+
+	i, j := 0, len(nums) - 1
+	for i < j {
+		m := (i + j) / 2
+		if nums[m] > nums[j] {
+			i = m + 1
+		} else if nums[m] < nums[j] {
+			j = m
+		} else {
+			j--
+		}
+	}
+	return nums[i]
+}
---
+++ Go-Solutions/jianzhi/11.go
@@ -0,0 +1,22 @@
+package jianzhi
+
+// Problem link: https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/
+// sol: https://leetcode-cn.com/leetbook/read/illustration-of-algorithm/5055b1/
+func minArray(numbers []int) int {
+	if numbers == nil || len(numbers) == 0 {
+		return -1
+	}
+
+	i, j := 0, len(numbers) - 1
+	for i < j {
+		m := (i + j) / 2
+		if numbers[m] > numbers[j] {
+			i = m + 1
+		} else if numbers[m] < numbers[j] {
+			j = m
+		} else {
+			j--
+		}
+	}
+	return numbers[i]
+}
