e1eab0ca0b0af7123b956634d06d19a3cb44b65d
James Xu <jamesxuhaozhe@gmail.com>
2020-09-28 16:08:30 +0800

    Add more solution for offer 3

--- Go-Solutions/jianzhi/3.go
+++ Go-Solutions/jianzhi/3.go
@@ -1,5 +1,7 @@
 package jianzhi
 
+import "sort"
+
 // Problem link: https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
 // O n
 func findRepeatNumber(nums []int) int {
@@ -17,3 +19,24 @@ func findRepeatNumber(nums []int) int {
 	return -1
 }
 
+func findRepeatNumber1(nums []int) int {
+	sort.Ints(nums)
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[i - 1] {
+			return nums[i]
+		}
+	}
+	return -1
+}
+
+func findRepeatNumber2(nums []int) int {
+	arr := make([]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		arr[nums[i]]++
+		if arr[nums[i]] > 1 {
+			return nums[i]
+		}
+	}
+
+	return -1
+}
