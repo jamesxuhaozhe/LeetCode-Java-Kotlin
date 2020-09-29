09039aa59014761c23ebae94182b4a5898d69f5e
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-09-29 22:46:53 +0800

    Add offer 6 problem

---
+++ Go-Solutions/jianzhi/6.go
@@ -0,0 +1,25 @@
+package jianzhi
+
+// Problem link: https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/
+func reversePrint(head *ListNode) []int {
+	if head == nil {
+		return []int{}
+	}
+
+	// do one pass
+	arr := make([]int, 0)
+	for head != nil {
+		arr = append(arr, head.Val)
+		head = head.Next
+	}
+
+	// start reversing the array
+	i, j := 0, len(arr) - 1
+	for i < j {
+		arr[i], arr[j] = arr[j], arr[i]
+		i++
+		j--
+	}
+
+	return arr
+}
