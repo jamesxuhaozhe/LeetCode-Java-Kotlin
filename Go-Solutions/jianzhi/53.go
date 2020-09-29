bd96cebfb9da1bbbb6945b8b63b114e649b75592
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-09-29 22:27:17 +0800

    Add missing number

--- Go-Solutions/jianzhi/53.go
+++ Go-Solutions/jianzhi/53.go
@@ -10,3 +10,16 @@ func missingNumber(nums []int) int {
 	}
 	return expected - actual
 }
+
+func missingNumber1(nums []int) int {
+	i, j := 0, len(nums) - 1
+	for i <= j {
+		m := (i + j) / 2
+		if nums[m] == m {
+			i = m + 1
+		} else {
+			j = m - 1
+		}
+	}
+	return i
+}
