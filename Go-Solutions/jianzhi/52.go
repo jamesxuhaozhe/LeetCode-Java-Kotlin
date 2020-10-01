3ae51540e6411b168a97dda14e8f41b340bf2c18
James Xu <jamesxuhaozhe@gmail.com>
2020-10-01 23:37:03 +0800

    Add 52

---
+++ Go-Solutions/jianzhi/52.go
@@ -0,0 +1,21 @@
+package jianzhi
+
+// sol: https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/solution/shuang-zhi-zhen-fa-lang-man-xiang-yu-by-ml-zimingm/
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	node1, node2 := headA, headB
+
+	for node1 != node2 {
+		if node1 != nil {
+			node1 = node1.Next
+		} else {
+			node1 = headB
+		}
+
+		if node2 != nil {
+			node2 = node2.Next
+		} else {
+			node2 = headA
+		}
+	}
+	return node1
+}
