f13348705bf484afdbfba377372d8474a01a2b91
James Xu <jamesxuhaozhe@gmail.com>
2020-10-13 23:03:20 +0800

    Add problem 1150 another solution

--- Go-Solutions/1150.go
+++ Go-Solutions/1150.go
@@ -9,3 +9,48 @@ func isMajorityElement(nums []int, target int) bool {
 	}
 	return count > len(nums)/2
 }
+
+func isMajorityElement1(nums []int, target int) bool {
+	farleftIndex, farRightIndex := getFarLeftIndex1150(nums, target), getFarRightIndex1150(nums, target)
+	if farleftIndex == -1 || farRightIndex == -1 {
+		return false
+	}
+
+	return (farRightIndex - farleftIndex + 1) > len(nums)/2
+}
+
+func getFarRightIndex1150(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)>>1
+		if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			if (mid == len(nums)-1) || (nums[mid+1] != target) {
+				return mid
+			}
+			left = mid + 1
+		}
+	}
+	return -1
+}
+
+func getFarLeftIndex1150(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)>>1
+		if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			if (mid == 0) || (nums[mid-1] != target) {
+				return mid
+			}
+			right = mid - 1
+		}
+	}
+	return -1
+}
