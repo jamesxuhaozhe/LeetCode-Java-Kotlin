1df68cf2d13488189be8f90769b53a545e3b1c31
James Xu <jamesxuhaozhe@gmail.com>
2020-10-14 23:03:12 +0800

    Add problem 374

---
+++ Go-Solutions/374.go
@@ -0,0 +1,21 @@
+package main
+
+// nothing fancy, just binary search
+func guessNumber(n int) int {
+	left, right := 1, n
+	for left <= right {
+		mid := left + (right - left) >> 1
+		if guess(mid) == 0 {
+			return mid
+		} else if guess(mid) == 1 {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return -1
+}
+
+func guess(mid int) int {
+	return 0
+}
