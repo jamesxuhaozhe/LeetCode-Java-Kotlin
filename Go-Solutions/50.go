6c6f5a48a916e77c3d8f53ffb4b25218a228f8ea
James Xu <jamesxuhaozhe@gmail.com>
2020-10-15 18:33:53 +0800

    Add problem 50

---
+++ Go-Solutions/50.go
@@ -0,0 +1,21 @@
+package main
+// solution: https://leetcode-cn.com/problems/powx-n/solution/powx-n-by-leetcode-solution/
+func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+
+	if n == 1 {
+		return x
+	}
+
+	if n < 0 {
+		n = -n
+		x = 1 / x
+	}
+	tmp := myPow(x, n/2)
+	if n%2 == 0 {
+		return tmp * tmp
+	}
+	return x * tmp * tmp
+}
