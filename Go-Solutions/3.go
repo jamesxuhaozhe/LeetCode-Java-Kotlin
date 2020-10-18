40c7e7c83daa3fcadeda5c86d1a9d90461800026
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-10-18 15:13:56 +0800

    Add problem 3

--- Go-Solutions/3.go
+++ Go-Solutions/3.go
@@ -12,3 +12,31 @@ func findRepeatNumber(nums []int) int {
 
 	return 0
 }
+
+// 滑动窗口
+func lengthOfLongestSubstring(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+
+	var flag [256]int
+	result, left, right := 0, 0, -1
+	for left < len(s) {
+		if right+1 < len(s) && flag[s[right+1]-'a'] == 0 {
+			flag[s[right+1]-'a']++
+			right++
+		} else {
+			flag[s[left]-'a']--
+			left++
+		}
+		result = max3(result, right-left+1)
+	}
+	return result
+}
+
+func max3(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
