3caef9fcc0e91e78e1ee2a79ff698731e9e63d91
James Xu <jamesxuhaozhe@gmail.com>
2020-10-20 19:42:37 +0800

    Add while solution for problem 1052

--- Go-Solutions/1052.go
+++ Go-Solutions/1052.go
@@ -92,17 +92,62 @@ func maxSatisfied_sliding_window(customers []int, grumpy []int, X int) int {
 	return max
 }
 
+
+func maxSatisfied_sliding_window_while(customers []int, grumpy []int, X int) int {
+	if customers == nil || len(customers) == 0 {
+		return 0
+	}
+
+	if grumpy == nil || len(grumpy) == 0 {
+		return 0
+	}
+
+	if len(customers) != len(grumpy) {
+		return 0
+	}
+
+	if X > len(customers) {
+		return 0
+	}
+
+	// 加入老板不采用必杀技的话，他应该有的人流量
+	baseCount := 0
+	for i := 0; i < len(customers); i++ {
+		if grumpy[i] == 0 {
+			baseCount += customers[i]
+		}
+	}
+
+	left, right := 0, 0
+	max := math.MinInt64
+	for right < len(customers) {
+		if grumpy[right] == 1 {
+			baseCount += customers[right]
+		}
+
+		if right - left == X {
+			if grumpy[left] == 1 {
+				baseCount -= customers[left]
+			}
+			left++
+		}
+		right++
+		max = max1052(max, baseCount)
+	}
+
+	return max
+}
 func max1052(a int, b int) int {
 	if a > b {
 		return a
 	}
 	return b
 }
-
-
+//
+//
 //func main() {
 //	cus := []int{1,0,1,2,1,1,7,5}
 //	grump := []int{0,1,0,1,0,1,0,1}
 //	X := 3
-//	fmt.Println(maxSatisfied_sliding_window(cus, grump, X))
+//	fmt.Println(maxSatisfied_sliding_window_while(cus, grump, X))
 //}
