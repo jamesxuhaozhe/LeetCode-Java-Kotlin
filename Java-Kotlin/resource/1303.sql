910f183edb663443b8240850c8da90a54437e8bf
James Xu <jamesxuhaozhe@gmail.com>
2020-11-12 23:06:01 +0800

    Add sql problem 1303

--- Java-Kotlin/resource/1303.sql
+++ Java-Kotlin/resource/1303.sql
@@ -1,3 +1,6 @@
 
 # 子查询
 select a.employee_id, (select count(*) from Employee b where b.team_id = a.team_id) as team_size from Employee a;
+
+# 左连接
+select e1.employee_id, count(*) team_size from Employee e1 left join Employee e2 on e1.team_id = e2.team_id group by e1.employee_id;
