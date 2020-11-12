ab0ab2d2a5c397e2de4ac45a538f483f30b918b2
James Xu <jamesxuhaozhe@gmail.com>
2020-11-12 21:22:51 +0800

    Add sql problem 1378

---
+++ Java-Kotlin/resource/1378.sql
@@ -0,0 +1,1 @@
+select unique_id, `name` from Employees left join EmployeeUNI using(id);
--- Java-Kotlin/resource/product_sales_analysis_1068.sql
+++ Java-Kotlin/resource/product_sales_analysis_1068.sql
@@ -1,3 +1,4 @@
 select b.product_name, a.`year`, a.`price` from Sales a join Product b on a.product_id = b.product_id;
 
+# better
 select `product_name`, `year`, `price` from `Sales` join `Product` using(`product_id`);
