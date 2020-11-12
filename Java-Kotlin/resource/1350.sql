906533a3b0c2f93dee02b7e206d98c7ecf92cb1b
James Xu <jamesxuhaozhe@gmail.com>
2020-11-12 21:52:26 +0800

    Add second sol for sql problem 1350

--- Java-Kotlin/resource/1350.sql
+++ Java-Kotlin/resource/1350.sql
@@ -1,3 +1,5 @@
 # 用子查询
 select s.id, s.`name` from Students as s where s.`department_id` not in (select d.`id` from Departments as d);
 
+# use left join
+select s.`id`, s.`name` from Students s left join Departments d on s.department_id = d.id where d.id is NULL;
