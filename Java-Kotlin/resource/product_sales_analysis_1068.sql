ef3c79c974ec4cc4cfca6324d7c2c8a12bf119e6
James Xu <jamesxuhaozhe@gmail.com>
2020-11-12 21:12:45 +0800

    Add solution for problem 1068

--- Java-Kotlin/resource/product_sales_analysis_1068.sql
+++ Java-Kotlin/resource/product_sales_analysis_1068.sql
@@ -1,1 +1,3 @@
 select b.product_name, a.`year`, a.`price` from Sales a join Product b on a.product_id = b.product_id;
+
+select `product_name`, `year`, `price` from `Sales` join `Product` using(`product_id`);
