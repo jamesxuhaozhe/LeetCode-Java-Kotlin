359960b8d4e3c048edf93a98d6083ee5e7c2a861
James Xu <jamesxuhaozhe@gmail.com>
2020-11-13 11:41:17 +0800

    Add problem 1173

--- Java-Kotlin/resource/1173.sql
+++ Java-Kotlin/resource/1173.sql
@@ -10,3 +10,6 @@ SELECT
 		) AS immediate_percentage
 	FROM
 		Delivery;
+
+
+select round((select count(*) from Delivery where order_date = customer_pref_delivery_date) / (select count(*) from Delivery) * 100,2) as immediate_percentage;
