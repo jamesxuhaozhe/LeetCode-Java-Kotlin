4a1d70da93f95e59845ff2af002fff8689b2a1b0
James Xu <jamesxuhaozhe@gmail.com>
2020-11-13 16:01:42 +0800

    Add problem 1294

---
+++ Java-Kotlin/resource/1294.sql
@@ -0,0 +1,1 @@
+select c.country_name, (case when avg(w.weather_state) <= 15 then 'Cold' when avg(w.weather_state) >= 25 then 'Hot' else 'Warm' end) as weather_type from Weather w left join Countries c on w.country_id = c.country_id where year(w.`day`) = 2019 and month(w.`day`) = 11 group by w.country_id;
