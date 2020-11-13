eedbdecbc40fc9da4bb84537db80b18e91026f84
James Xu <jamesxuhaozhe@gmail.com>
2020-11-13 12:08:59 +0800

    Add problem 586

---
+++ Java-Kotlin/resource/586.sql
@@ -0,0 +1,1 @@
+select customer_number from orders group by customer_number having count(order_number) = (select count(order_number) from orders group by customer_number order by count(order_number) desc limit 1);
