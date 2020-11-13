6764d853a99515140d447c1b394befce842f61d8
James Xu <jamesxuhaozhe@gmail.com>
2020-11-13 14:43:10 +0800

    Add problem 1327

---
+++ Java-Kotlin/resource/1327.sql
@@ -0,0 +1,1 @@
+select p.product_name, sum(o.unit) as unit from Orders o join Products p on o.product_id = p.product_id where year(o.order_date) = 2020 and month(o.order_date) = 2 group by p.product_name having unit >= 100;
