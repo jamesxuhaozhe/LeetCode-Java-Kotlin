0bda97fe0e03d445ab049a0ae05fbd8681bd8f9a
James Xu <jamesxuhaozhe@gmail.com>
2020-11-13 14:59:17 +0800

    Add problem 183

--- Java-Kotlin/resource/183.sql
+++ Java-Kotlin/resource/183.sql
@@ -4,3 +4,7 @@ FROM
 	Customers c
 WHERE
 	c.Id NOT IN ( SELECT o.CustomerId FROM Orders o );
+
+	select c.`Name` as Customers from Customers c left join Orders o on c.Id = o.CustomerId where o.CustomerId is NULL;
+
+select `Name` as `Customers` from Customers where Id not in (select CustomerId from Orders);
