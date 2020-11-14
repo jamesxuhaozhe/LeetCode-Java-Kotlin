2d33ef34621bb3125cc12fbb628c0a89b230d3c1
James Xu <jamesxuhaozhe@gmail.com>
2020-11-15 00:04:05 +0800

    Add problem 197

--- Java-Kotlin/resource/176.sql
+++ Java-Kotlin/resource/176.sql
@@ -11,3 +11,6 @@ SELECT
 		),
 NULL
 	) AS SecondHighestSalary;
+
+
+select ifnull((select distinct Salary from Employee order by Salary desc limit 1, 1), null) as SecondHighestSalary;
---
+++ Java-Kotlin/resource/197.sql
@@ -0,0 +1,1 @@
+select distinct w1.id from Weather w1, Weather w2 where datediff(w1.recordDate, w2.recordDate) = 1 and w1.Temperature > w2.Temperature;
