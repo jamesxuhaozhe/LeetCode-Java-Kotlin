14a8a6fb3e624b4be9f079652e6fd4d84f798ce2
James Xu <jamesxuhaozhe@gmail.com>
2021-01-05 16:51:49 +0800

    just do 206 go solution the second time

--- Go-Solutions/206.go
+++ Go-Solutions/206.go
@@ -1,6 +1,17 @@
 package main
 
-func reverseList(head *ListNode) *ListNode {
+func reverseList0(head *ListNode) *ListNode {
+	var newHead *ListNode = nil
+	for head != nil {
+		next := head.Next
+		head.Next = newHead
+		newHead = head
+		head = next
+	}
+	return newHead
+}
+
+func reverseList1(head *ListNode) *ListNode {
 	var newHead *ListNode = nil
 	for head != nil {
 		next := head.Next
