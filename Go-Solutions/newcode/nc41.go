da63dcec14e4d8fcdbc0507e3df0733a614055b5
James Xu <jamesxuhaozhe@gmail.com>
2021-01-18 20:53:49 +0800

    Add nc41

---
+++ Go-Solutions/newcode/nc41.go
@@ -0,0 +1,21 @@
+package newcode
+
+func maxLength(arr []int) int {
+	// write code here
+	hm := make(map[int]int)
+	left, right := 0, 0
+	res := 0
+	for right < len(arr) {
+		if idx, ok := hm[arr[right]]; ok {
+			if idx >= left {
+				left = idx + 1
+			}
+		}
+		if right-left+1 > res {
+			res = right - left + 1
+		}
+		hm[arr[right]] = right
+		right++
+	}
+	return res
+}
