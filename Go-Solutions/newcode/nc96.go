b2596216e724d8b414fb9dbd71b6a1816878dd2e
James Xu <jamesxuhaozhe@gmail.com>
2021-01-19 22:50:04 +0800

    Add nc96

---
+++ Go-Solutions/newcode/nc96.go
@@ -0,0 +1,26 @@
+package newcode
+
+func isPail96( head *ListNode ) bool {
+	// write code here
+	reversed := reverse96(head)
+	for reversed != nil && head != nil {
+		if reversed.Val != head.Val {
+			return false
+		}
+		reversed = reversed.Next
+		head = head.Next
+	}
+	return true
+}
+
+func reverse96(head *ListNode) *ListNode {
+	res := &ListNode{Val: -1, Next: nil,}
+
+	p := head
+	for p != nil {
+		t := &ListNode{Val: p.Val, Next: res.Next,}
+		res.Next = t
+		p = p.Next
+	}
+	return res.Next
+}
