9a6c58189345b99568339c122c55b8c24dc52070
James Xu <jamesxuhaozhe@gmail.com>
2021-01-20 22:52:15 +0800

    Add nc23

---
+++ Go-Solutions/nowcoder/nc23.go
@@ -0,0 +1,24 @@
+package nowcoder
+
+func partition23( head *ListNode ,  x int ) *ListNode {
+	// write code here
+	if head == nil {
+		return nil
+	}
+	lessHead, moreHead := &ListNode{Val: -1, Next: nil,}, &ListNode{Val: -1, Next: nil,}
+	lessRunner, moreRunner := lessHead, moreHead
+	runner := head
+	for runner != nil {
+		if runner.Val < x {
+			lessRunner.Next = runner
+			lessRunner = lessRunner.Next
+		} else {
+			moreRunner.Next = runner
+			moreRunner = moreRunner.Next
+		}
+		runner = runner.Next
+	}
+	moreRunner.Next = nil
+	lessRunner.Next = moreHead.Next
+	return lessHead.Next
+}
