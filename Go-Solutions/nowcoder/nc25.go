838efb06b3628d35cf6712ae379f5612f206dc8a
Xu Haozhe <jamesxuhaozhe@gmail.com>
2021-01-22 18:11:17 +0800

    Add nc25

---
+++ Go-Solutions/nowcoder/nc25.go
@@ -0,0 +1,17 @@
+package nowcoder
+
+func deleteDuplicates25(head *ListNode) *ListNode {
+	// write code here
+	if head == nil || head.Next == nil {
+		return head
+	}
+	cur := head
+	for cur.Next != nil {
+		if cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return head
+}
