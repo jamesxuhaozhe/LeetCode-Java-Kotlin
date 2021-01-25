0c13da5d26c22c3c7ef2e2eb1dc8265c6e0f2a40
Xu Haozhe <jamesxuhaozhe@gmail.com>
2021-01-25 23:18:20 +0800

    Add nc89

---
+++ Java-Kotlin/src/main/java/nowcoder/nc89.java
@@ -0,0 +1,56 @@
+package nowcoder;
+
+public class nc89 {
+
+    private static final int DIFF = 'A' - 'a';
+
+    public String trans(String s, int n) {
+        // write code here
+
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+
+        String[] arr = s.split(" ", -1);
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            String temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+
+        rebaseArr(arr);
+        return String.join(" ", arr);
+    }
+
+    private void rebaseArr(String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rebaseStr(arr[i]);
+        }
+    }
+
+    private String rebaseStr(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            sb.append(rebaseCh(str.charAt(i)));
+        }
+        return sb.toString();
+    }
+
+    private char rebaseCh(char ch) {
+        if (ch >= 'a' && ch <= 'z') {
+            return (char) (ch + DIFF);
+        } else if (ch >= 'A' && ch <= 'Z') {
+            return (char) (ch - DIFF);
+        } else {
+            return ch;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new nc89().trans("This is a sample", 16));
+    }
+}
