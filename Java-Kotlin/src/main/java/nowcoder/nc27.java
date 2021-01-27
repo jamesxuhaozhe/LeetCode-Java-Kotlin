16090cedce0fae0a9b1de3ae4e029d24fe2d33ed
Xu Haozhe <jamesxuhaozhe@gmail.com>
2021-01-27 13:08:48 +0800

    Add nc27 java

---
+++ Java-Kotlin/src/main/java/nowcoder/nc27.java
@@ -0,0 +1,27 @@
+package nowcoder;
+
+import java.util.ArrayList;
+
+public class nc27 {
+
+    public ArrayList<ArrayList<Integer>> subsets(int[] num) {
+        if (num == null || num.length == 0) {
+            return new ArrayList<>();
+        }
+
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        dfs(num, res, list, 0);
+
+        return res;
+    }
+
+    private static void dfs(int[] num, ArrayList<ArrayList<Integer>> res, ArrayList<Integer> list, int begin) {
+        res.add(new ArrayList<>(list));
+        for (int i = begin; i < num.length; i++) {
+            list.add(num[i]);
+            dfs(num, res, list, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+}
