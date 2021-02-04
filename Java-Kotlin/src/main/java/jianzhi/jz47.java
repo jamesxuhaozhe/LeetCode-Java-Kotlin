bdf573bb729336440b9ec929841a8f852864dc61
Xu Haozhe <jamesxuhaozhe@gmail.com>
2021-02-04 23:51:30 +0800

    add jz47

---
+++ Java-Kotlin/src/main/java/jianzhi/jz47.java
@@ -0,0 +1,10 @@
+package jianzhi;
+
+public class jz47 {
+
+    public int Sum_Solution(int n) {
+        int sum = n;
+        boolean b = (n > 0) && ((sum += Sum_Solution(n - 1)) > 0);
+        return sum;
+    }
+}
