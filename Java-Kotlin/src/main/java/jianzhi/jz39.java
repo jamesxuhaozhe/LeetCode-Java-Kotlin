04618f3c54431388fae5f30ebe678675165c87bc
Xu Haozhe <jamesxuhaozhe@gmail.com>
2021-02-04 15:55:34 +0800

    add jz39

---
+++ Java-Kotlin/src/main/java/jianzhi/jz39.java
@@ -0,0 +1,39 @@
+package jianzhi;
+
+import common.datastructure.TreeNode;
+
+public class jz39 {
+
+    // see jianzhi book solution
+    public boolean IsBalanced_Solution(TreeNode root) {
+        IntHolder depth = new IntHolder(0);
+        return isBalanced(root, depth);
+    }
+
+    private static boolean isBalanced(TreeNode node, IntHolder depth) {
+        if (node == null) {
+            depth.value = 0;
+            return true;
+        }
+
+        IntHolder leftDepth = new IntHolder(0);
+        IntHolder rightDepth = new IntHolder(0);
+        if (!isBalanced(node.left, leftDepth) || !isBalanced(node.right, rightDepth)) {
+            return false;
+        }
+
+        if (Math.abs(leftDepth.value - rightDepth.value) <= 1) {
+            depth.value = 1 + Math.max(leftDepth.value, rightDepth.value);
+            return true;
+        }
+
+        return false;
+    }
+
+    private static class IntHolder {
+        private int value;
+        private IntHolder(int value) {
+            this.value = value;
+        }
+    }
+}
