46099e66c759e76f453d374a8e6c779073ede117
Xu Haozhe <jamesxuhaozhe@gmail.com>
2021-02-04 17:43:46 +0800

    Add jz41

---
+++ Java-Kotlin/src/main/java/jianzhi/jz41.java
@@ -0,0 +1,41 @@
+package jianzhi;
+
+import java.util.ArrayList;
+
+public class jz41 {
+
+    public ArrayList<ArrayList<Integer>> FindContinuousSequence(int sum) {
+        if (sum < 1) {
+            return new ArrayList<>();
+        }
+
+        int left = 1;
+        int right = 2;
+        int curSum = left + right;
+        int cutPoint = (sum + 1) / 2;
+
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        while (left < right && right <= cutPoint) {
+            if (curSum < sum) {
+                right++;
+                curSum += right;
+            } else if (curSum > sum) {
+                curSum -= left;
+                left++;
+            } else {
+                res.add(getRangeList(left, right));
+                curSum -= left;
+                left++;
+            }
+        }
+        return res;
+    }
+
+    private ArrayList<Integer> getRangeList(int left, int right) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = left; i <= right; i++) {
+            result.add(i);
+        }
+        return result;
+    }
+}
