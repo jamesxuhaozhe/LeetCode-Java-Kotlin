efa4a89d1d6c2fa98ef630247399123d887f73ef
Xu Haozhe <jamesxuhaozhe@gmail.com>
2021-02-04 20:06:12 +0800

    Add jz43

---
+++ Java-Kotlin/src/main/java/jianzhi/jz43.java
@@ -0,0 +1,27 @@
+package jianzhi;
+
+public class jz43 {
+
+    // 看jianzhi offer 书，取巧罢了
+    public String LeftRotateString(String str, int n) {
+        if (str == null || str.length() == 0 || n == 0 || (n % str.length()) == 0) {
+            return str;
+        }
+
+        char[] chars = str.toCharArray();
+        reverse(chars, 0, n - 1);
+        reverse(chars, n, chars.length - 1);
+        reverse(chars, 0, chars.length - 1);
+        return new String(chars);
+    }
+
+    private static void reverse(char[] chars, int start, int end) {
+        while (start < end) {
+            char tmp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+}
