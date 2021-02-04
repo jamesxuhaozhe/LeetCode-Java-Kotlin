9876e75ba878c67de22d0470f036f7ccce848351
Xu Haozhe <jamesxuhaozhe@gmail.com>
2021-02-05 00:06:23 +0800

    Add nc111

---
+++ Java-Kotlin/src/main/java/nowcoder/nc111.java
@@ -0,0 +1,39 @@
+package nowcoder;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+// 跟jianzhi的题目差不多
+public class nc111 {
+
+    public String solve (int[] nums) {
+        // write code here
+
+        if (nums == null || nums.length == 0) {
+            return "";
+        }
+
+        String[] strs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strs[i] = nums[i] + "";
+        }
+        Arrays.sort(strs, new Comparator<String>() {
+            public int compare(String o1, String o2) {
+                String s1 = o1 + o2;
+                String s2 = o2 + o1;
+                return s2.compareTo(s1);
+            }
+        });
+
+        // 这里是唯一需要注意的
+        if (strs[0].equals("0")) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String s : strs) {
+            sb.append(s);
+        }
+
+        return sb.toString();
+    }
+}
