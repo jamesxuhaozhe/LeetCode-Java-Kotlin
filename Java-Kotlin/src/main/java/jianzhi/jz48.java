7e985af4d3f3a4c3d898d84cb91d610a29fe9f3f
Xu Haozhe <jamesxuhaozhe@gmail.com>
2021-02-05 14:23:08 +0800

    Add jz48

---
+++ Java-Kotlin/src/main/java/jianzhi/jz48.java
@@ -0,0 +1,17 @@
+package jianzhi;
+
+public class jz48 {
+
+    // see jianzhi offer book
+    public int Add(int num1, int num2) {
+        int sum = 0;
+        int carry = 0;
+        do {
+            sum = num1 ^ num2;
+            carry = (num1 & num2) << 1;
+            num1 = sum;
+            num2 = carry;
+        } while (num2 != 0);
+        return num1;
+    }
+}
