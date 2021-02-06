d5bcba23e6634bdc591380c8aea611d3b11c7661
Xu Haozhe <jamesxuhaozhe@gmail.com>
2021-02-06 23:07:24 +0800

    Add jz58

---
+++ Java-Kotlin/src/main/java/jianzhi/jz58.java
@@ -0,0 +1,30 @@
+package jianzhi;
+
+import common.datastructure.TreeNode;
+
+public class jz58 {
+
+    boolean isSymmetrical(TreeNode pRoot) {
+        if (pRoot == null) {
+            return true;
+        }
+
+        return isSymm(pRoot.left, pRoot.right);
+    }
+
+    private static boolean isSymm(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+
+        if (root1.val != root2.val) {
+            return false;
+        }
+
+        return isSymm(root1.left, root2.right) && isSymm(root1.right, root2.left);
+    }
+}
