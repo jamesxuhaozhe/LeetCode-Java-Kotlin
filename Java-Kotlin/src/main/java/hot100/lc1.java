cc5ca90eb56dd42ccc555d7fe41735650d4b1a82
James Xu <jamesxuhaozhe@gmail.com>
2021-02-10 16:12:12 +0800

    Add problem hot100 lc1

---
+++ Java-Kotlin/src/main/java/hot100/lc1.java
@@ -0,0 +1,19 @@
+package hot100;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class lc1 {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && (map.get(complement) != i)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
