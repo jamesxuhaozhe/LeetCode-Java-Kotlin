e4744a56ee2cf8133c000971ffcf87cdb3fc30cb
James Xu <jamesxuhaozhe@gmail.com>
2021-02-14 21:46:55 +0800

    Add comments

--- Java-Kotlin/src/main/java/hot100/lc1.java
+++ Java-Kotlin/src/main/java/hot100/lc1.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 public class lc1 {
 
+    //classic two sum problem
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
