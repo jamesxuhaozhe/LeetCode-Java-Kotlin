bb2973b8c7a10ca1906f85c7e89fb6ebeae91a40
James Xu <jamesxuhaozhe@gmail.com>
2021-02-15 22:16:56 +0800

    Add lc141

---
+++ Java-Kotlin/src/main/java/hot100/lc141.java
@@ -0,0 +1,23 @@
+package hot100;
+
+import common.datastructure.ListNode;
+
+public class lc141 {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
