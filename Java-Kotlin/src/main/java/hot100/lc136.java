896266a15623938aa7d32da9f2008a30fdc05399
James Xu <jamesxuhaozhe@gmail.com>
2021-02-15 14:07:13 +0800

    Add lc136

---
+++ Java-Kotlin/src/main/java/hot100/lc136.java
@@ -0,0 +1,12 @@
+package hot100;
+
+public class lc136 {
+
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
