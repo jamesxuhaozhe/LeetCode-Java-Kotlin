34dc0ce238c7dc6a1d0aeab4c25491d1ebd9b7ef
James Xu <jamesxuhaozhe@gmail.com>
2021-02-15 14:03:31 +0800

    Add lc104

---
+++ Java-Kotlin/src/main/java/hot100/lc104.java
@@ -0,0 +1,14 @@
+package hot100;
+
+import common.datastructure.TreeNode;
+
+public class lc104 {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
