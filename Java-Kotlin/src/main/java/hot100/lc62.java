4eace1740ab22a90f924eb9989c6dfc2339dcb2d
James Xu <jamesxuhaozhe@gmail.com>
2021-02-15 22:57:24 +0800

    Add lc62

---
+++ Java-Kotlin/src/main/java/hot100/lc62.java
@@ -0,0 +1,28 @@
+package hot100;
+
+public class lc62 {
+
+    public int uniquePaths(int m, int n) {
+        if (m < 1 || n < 1) {
+            return 0;
+        }
+
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    continue;
+                }
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
